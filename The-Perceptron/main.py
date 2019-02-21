a29f7399014a93d6af7884ef7423a031b665f103
Gregory Eales <gregoeales@aol.com>
2019-02-20 22:23:51 -0800

    additions

--- The-Perceptron/main.py
+++ The-Perceptron/main.py
@@ -1,10 +1,13 @@
 from perceptron import Perceptron
 from abalone_data import get_abalone
+from matplotlib import pyplot as plt
 
 
 x, y = get_abalone()
 
 model = Perceptron(input_shape = [1, 9])
 
-model.train(x, y, 0.0001, 1)
+model.train(x, y, 0.01, 100)
 
+plt.plot(model.historical_error)
+plt.show()
--- The-Perceptron/perceptron.py
+++ The-Perceptron/perceptron.py
@@ -1,31 +1,39 @@
 import numpy as np
 import time
+from tqdm import tqdm
 
 class Perceptron(object):
 	
 	def __init__(self, input_shape = [1, 2]):
-		self.w = np.random.random(input_shape) / 100000
+		self.w = np.random.random(input_shape) / 100
+		self.w = self.w[0]
+		self.error = 0
+		self.historical_error = []
 
 	def train(self, x, y, alpha, iterations):
-		for iteration in range(iterations):
+		print("Training Perceptron Model")
+		for iteration in tqdm(range(iterations)):
+			self.error = 0
 			for i in range(x.shape[0]):
 				z = np.dot(self.w, x[i].T)
-				print(z)
-				
+
 				if z < 0.5:
 					z = 0
 
 				else:
 					z = 1
 
-				self.w = self.w - self.cost(y[i], z, x[i], alpha)
+				self.error = self.error + abs(y - z)
+				self.w = self.w + self.cost(y[i], z, x[i], alpha)
+
+			self.historical_error.append(self.error)
 
 	def cost(self, z, y, x, alpha):
-		return alpha * x * (z - y)
+		return alpha * x * (y - z)
 		
 
 	def predict(self, x):
-		z = np.dot(self.w.T, x[i])
+		z = np.dot(self.w, x.T)
 		if z < 0.5:
 			return 0
 
