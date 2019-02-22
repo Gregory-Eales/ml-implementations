0d3c9a4f040061a27c3ec8210ccc1118b88290f2
Gregory Eales <gregoeales@aol.com>
2019-02-21 23:54:32 -0800

    Update logistic_regression.py

--- Logistic-Regression/logistic_regression.py
+++ Logistic-Regression/logistic_regression.py
@@ -11,20 +11,22 @@ class LogisticRegressor(object):
 		print("    ------------------------------------------")
 		print("    |   Training Logistic Regression Model   |")
 		print("    ------------------------------------------")
-		for iteration in tqdm(range(iterations)):
-			sleep(0.1)
+		for i in tqdm(range(iterations)):
+			z = np.dot(self.w, x.T)
+			a = self.sigmoid(z)
 
-	def cost(self):
-		pass
+	def cost(self, y_hat, y):
+		return y*np.log(y_hat) - (1-y)*np.log(1-y_hat)
 
 	def cost_prime(self):
 		pass
+  
+	def sigmoid(self, z):
+		return 1/(1+np.exp(-z))
 
-	def sigmoid(self):
-		pass
+	def sig_prime(self, z):
+		return self.sigmoid(z)*(1 - self.sigmoid(z))
 
-	def sig_prime(self):
-		pass
-
-	def predict(self):
-		pass
+	def predict(self, x):
+		return self.sigmoid(np.dot(self.w, x.T))
+		
