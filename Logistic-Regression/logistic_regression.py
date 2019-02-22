9794ae7f5c6ad2abbe63ef667e009ca8b949ff70
Gregory Eales <gregoeales@aol.com>
2019-02-21 23:38:18 -0800

    loading text during training

--- Logistic-Regression/logistic_regression.py
+++ Logistic-Regression/logistic_regression.py
@@ -1,12 +1,18 @@
 import numpy as np 
+from tqdm import tqdm
+from time import sleep
 
 class LogisticRegressor(object):
 
-	def __init__(self):
-		pass
+	def __init__(self, input_dim=[1, 2]):
+		self.w = np.random.random(input_dim)
 
-	def train(self):
-		pass
+	def train(self, x, y, iterations, alpha):
+		print("    ------------------------------------------")
+		print("    |   Training Logistic Regression Model   |")
+		print("    ------------------------------------------")
+		for iteration in tqdm(range(iterations)):
+			sleep(0.1)
 
 	def cost(self):
 		pass
--- Logistic-Regression/main.py
+++ Logistic-Regression/main.py
@@ -1,2 +1,5 @@
 import data
 import logistic_regression
+
+regressor = logistic_regression.LogisticRegressor([1, 2])
+regressor.train(1, 2, 100, 1)
