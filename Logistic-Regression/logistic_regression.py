d6324a0f14b35f0d3127b7b701c194c5d7991294
Gregory Eales <gregoeales@aol.com>
2019-02-22 02:20:32 -0800

    debugging regressor

--- Logistic-Regression/logistic_regression.py
+++ Logistic-Regression/logistic_regression.py
@@ -5,28 +5,34 @@ from time import sleep
 class LogisticRegressor(object):
 
 	def __init__(self, input_dim=[1, 2]):
-		self.w = np.random.random(input_dim)
+		self.w = np.random.random(input_dim)[0]
+		self.b = 0
+		self.historical_cost = []
 
 	def train(self, x, y, iterations, alpha):
 		print("    ------------------------------------------")
 		print("    |   Training Logistic Regression Model   |")
 		print("    ------------------------------------------")
-		for i in tqdm(range(iterations)):
-			z = np.dot(self.w, x.T)
-			a = self.sigmoid(z)
+		
+		for iteration in tqdm(range(iterations)):
+			error = 0
+			for i in range(x.shape[0]):
+				z = np.dot(self.w, x[i].T) + self.b
+				a = self.sigmoid(z)
+				error = error + self.cost(a, y[i])
+				self.w = self.w - alpha * x[i] * self.sig_prime(z) * self.cost(a, y[i])
+				self.b = self.b -  alpha * self.sig_prime(z) * self.cost(a, y[i])
+			self.historical_cost.append(error)
 
 	def cost(self, y_hat, y):
-		return y*np.log(y_hat) - (1-y)*np.log(1-y_hat)
+		return y_hat - y
 
-	def cost_prime(self):
-		pass
   
 	def sigmoid(self, z):
 		return 1/(1+np.exp(-z))
 
 	def sig_prime(self, z):
-		return self.sigmoid(z)*(1 - self.sigmoid(z))
+		return self.sigmoid(z)*(1-self.sigmoid(z))
 
 	def predict(self, x):
 		return self.sigmoid(np.dot(self.w, x.T))
-		
--- Logistic-Regression/main.py
+++ Logistic-Regression/main.py
@@ -1,5 +1,62 @@
 import data
 import logistic_regression
+import numpy as np
+from matplotlib import pyplot as plt
+import random
+
+
+x = []
+y = []
+
+for i in range(20):
+	num1 = random.gauss(10, 0.3)
+	num2 = random.gauss(1, 0.3)
+	x.append([num2, num1])
+	y.append([1])
+
+for i in range(20):
+	num1 = random.gauss(5, 0.3)
+	num2 = random.gauss(5, 0.3)
+	x.append([num2, num1])
+	y.append([0])
+
+x = np.array(x)
+y = np.array(y)
+
+
+
 
 regressor = logistic_regression.LogisticRegressor([1, 2])
-regressor.train(1, 2, 100, 1)
+
+regressor.train(x, y, 1000, 0.01)
+
+
+
+print(regressor.predict(np.array([5, 5])))
+print(regressor.predict(np.array([3, 10])))
+
+
+
+db1 = []
+db2 = []
+
+
+for i in range(700):
+	for j in range(700):
+		prediction = regressor.predict(np.array([i/25, j/25]))
+		if prediction < 0.53 and prediction > 0.48:
+			db1.append(i/50)
+			db2.append(j/50)
+
+
+
+#plt.plot(regressor.historical_cost)
+plt.scatter(db1, db2)
+plt.scatter(x[:, 0], x[:, 1])
+plt.show()
+
+
+
+
+
+
