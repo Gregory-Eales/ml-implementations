3430597d538bc8aa5aa77881e75c47abb8ed69a5
Gregory Eales <gregoeales@aol.com>
2019-02-26 17:05:58 -0600

    finished prediction method

--- Deep-Neural-Networks/neural_network.py
+++ Deep-Neural-Networks/neural_network.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import tensorflow as tf
 from tqdm import tqdm
+import time
 
 
 # TO DO:
@@ -21,13 +22,16 @@ class NeuralNetwork(object):
 
 		self.w = {}
 		self.b = {}
-		self.create_weights(num_layers)
+		self.a = {}
+		self.z = {}
+		self.num_layers = num_layers
+		self.create_weights()
 
 
 	def create_weights(self, num_layers=3, input_shape=[1, 1], output_shape=[1, 1]):
 
 		# make sure there are three or more layers
-		if num_layers < 3:
+		if self.num_layers < 3:
 			raise Exception("num_layers should not be less than 3, value given was: " + str(num_layers))
 		
 			# create first set
@@ -35,7 +39,7 @@ class NeuralNetwork(object):
 			self.b["b1"] = np.random.rand(input_shape[1]+1, 1)
 
 			# create hidden layers
-			for i in range(2, num_layers):
+			for i in range(2, self.num_layers):
 
 				self.w["w" + str(i)] = np.random.rand(input_shape[1]+1, input_shape[1]+1)
 				self.b["b" + str(i)] = np.random.rand(input_shape[1]+1, 1)
@@ -48,12 +52,27 @@ class NeuralNetwork(object):
 	def train(self, x, y, iterations=1, alpha=0.01):
 
 		# loop through data set each iteration
-		for i in range(iterations):
-			pass
+		print("    Training NeuralNetwork")
+		for i in tqdm(range(iterations)):
+			time.sleep(0.1)
 
 
 	def predict(self, x):
-		pass
+
+		# multiply then activate first layer
+		self.z["z1"] = self.dot(self.w["w1"], x.T) + self.b["b1"]
+		self.a["a1"] = self.tanh(self.z["z1"])
+
+		# loop through hidden layers, multiply then activate
+		for i in range(2, self.num_layers):
+			self.z["z" + str(i)] = self.dot(self.w["w"+str(i)], self.a["a" + str(i-1)])
+			self.a["a" + str(i)] = self.tanh(self.z["z" + str(i)])
+
+		# multiply then activate last layer
+		self.z["z" + str(self.num_layers)] = self.dot(self.w["w"+str(self.num_layers)], self.a["a" + str(self.num_layers-1)])
+		self.a["a" + str(self.num_layers)] = self.sigmoid(self.z["z" + str(self.num_layers)])
+
+		return self.a["a" + str(self.num_layers)]
 		
 
 	def sigmoid(self, z):
@@ -93,9 +112,23 @@ class NeuralNetwork(object):
 		else:
 			return 1 - np.square(np.tanh(z))
 
+	def relu(self, z):
+
+		if True:
+			return np.where(z > 0, 1, 0)
+
+		else:
+			None
 
 
 
+	self dot(self, a, b):
+
+		if True:
+			return tf.matmul(a, b)
+
+		else:
+			np.dot(a, b)
 
 
 
