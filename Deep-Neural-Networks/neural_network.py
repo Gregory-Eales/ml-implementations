73e5bb67418e7666297e8a8231e69cc37f8d0e4a
Gregory Eales <gregoeales@aol.com>
2019-02-26 19:19:36 -0600

    added type specification

--- Deep-Neural-Networks/neural_network.py
+++ Deep-Neural-Networks/neural_network.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import time
 
 
+
 # TO DO:
 # 	- create weights based on data, and topology
 # 	- pick activation function for each layer
@@ -18,35 +19,47 @@ import time
 class NeuralNetwork(object):
 
 
-	def __init__(self, num_layers=3, input_shape=[1, 1]):
+	def __init__(self, num_layers=3, input_shape=[1, 1], output_shape=[1,1]):
 
 		self.w = {}
 		self.b = {}
 		self.a = {}
 		self.z = {}
 		self.num_layers = num_layers
+		self.input_shape = input_shape
+		self.output_shape = output_shape
+		self.num_type = np.float64
 		self.create_weights()
+    
 
 
-	def create_weights(self, num_layers=3, input_shape=[1, 1], output_shape=[1, 1]):
+	def create_weights(self):
+
+		output_shape = self.output_shape
+		input_shape = self.input_shape
 
 		# make sure there are three or more layers
 		if self.num_layers < 3:
 			raise Exception("num_layers should not be less than 3, value given was: " + str(num_layers))
 		
-			# create first set
-			self.w["w1"] = np.random.rand(input_shape[1]+1, input_shape[1])
-			self.b["b1"] = np.random.rand(input_shape[1]+1, 1)
+		# create first set
+		self.w["w1"] = np.random.rand(input_shape[1]+1, input_shape[1])
+		self.w["w1"].dtype = self.num_type
+		self.b["b1"] = np.random.rand(input_shape[1]+1, 1)
+		self.b["b1"].dtype = self.num_type
+		# create hidden layers
+		for i in range(2, self.num_layers):
 
-			# create hidden layers
-			for i in range(2, self.num_layers):
+			self.w["w" + str(i)] = np.random.rand(input_shape[1]+1, input_shape[1]+1)
+			self.w["w" + str(i)].dtype = self.num_type
+			self.b["b" + str(i)] = np.random.rand(input_shape[1]+1, 1)
+			self.b["b" + str(i)].dtype = self.num_type
 
-				self.w["w" + str(i)] = np.random.rand(input_shape[1]+1, input_shape[1]+1)
-				self.b["b" + str(i)] = np.random.rand(input_shape[1]+1, 1)
-
-			# create last layer
-			self.w["w" + str(self.num_layers)] = np.random.rand(output_shape[1], input_shape[1]+1)
-			self.b["b" + str(self.num_layers)] = np.random.rand(output_shape[1], 1)
+		# create last layer
+		self.w["w" + str(self.num_layers)] = np.random.rand(output_shape[1], input_shape[1]+1)
+		self.w["w" + str(self.num_layers)].dtype = self.num_type
+		self.b["b" + str(self.num_layers)] = np.random.rand(output_shape[1], 1)
+		self.b["b" + str(self.num_layers)].dtype = self.num_type
 
 
 	def train(self, x, y, iterations=1, alpha=0.01):
@@ -59,8 +72,15 @@ class NeuralNetwork(object):
 
 	def predict(self, x):
 
+		xT = x.T
+		xT.dtype = self.num_type
+
 		# multiply then activate first layer
-		self.z["z1"] = self.dot(self.w["w1"], x.T) + self.b["b1"]
+		print(" W ", self.w["w1"])
+		print("b", self.b["b1"])
+		print("X.", xT)
+
+		self.z["z1"] = self.dot(self.w["w1"], xT) + self.b["b1"]
 		self.a["a1"] = self.tanh(self.z["z1"])
 
 		# loop through hidden layers, multiply then activate
@@ -77,17 +97,22 @@ class NeuralNetwork(object):
 
 	def sigmoid(self, z):
 		
+		one = np.ones(1, dtype=self.num_type)
+
+
 		# use library based on matrix size and GPU access
 		if True:
-			return tf.divide(1.000, tf.add(1.000, tf.exp(-z)))
+			return tf.divide(one, tf.add(one, tf.exp(-z)))
 
 		else:
-			return 1.000 / (1.000 + np.exp(-z))
+			return one / (one + np.exp(-z))
 
 	def sigmoid_prime(self, z):
 
+		one = np.ones(1, dtype=self.num_type)
+
 		if True:
-			return tf.multiply(self.sigmoid(z), (tf.sub(1, self.sigmoid(z))))
+			return tf.multiply(self.sigmoid(z), (tf.sub(one, self.sigmoid(z))))
 
 		else:
 			return self.sigmoid(z)*(1-self.sigmoid(z))
@@ -104,13 +129,14 @@ class NeuralNetwork(object):
 
 
 	def tanh_prime(self, z):
+		one = np.ones(1, dtype=self.num_type)
 
 		# use library based on matrix size and GPU access
 		if True:
-			return 1 - tf.square(tf.tanh(z))
+			return one - tf.square(tf.tanh(z))
 
 		else:
-			return 1 - np.square(np.tanh(z))
+			return one - np.square(np.tanh(z))
 
 	def relu(self, z):
 
@@ -122,7 +148,7 @@ class NeuralNetwork(object):
 
 
 
-	self dot(self, a, b):
+	def dot(self, a, b):
 
 		if True:
 			return tf.matmul(a, b)
@@ -133,6 +159,17 @@ class NeuralNetwork(object):
 
 
 
+nn = NeuralNetwork(input_shape = [1, 2])
+
+x = np.ones([1, 2], dtype=nn.num_type)
+
+t = time.time()
+
+for i in range(1):
+  nn.predict(x)
+
+
+print("Time: " + str(time.time() - t))
 
 
 
