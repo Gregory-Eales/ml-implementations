a44f22cfc0e002414ff37732344f4494e601a837
Gregory Eales <gregoeales@aol.com>
2019-02-28 11:38:44 -0600

    added pad method

--- Deep-Convolutional-Neural-Network/cnn.py
+++ Deep-Convolutional-Neural-Network/cnn.py
@@ -1,6 +1,6 @@
 import numpy as np
-import pytorch as pt
-
+import torch
+from tqdm import tqdm
 
 class CNN(object):
 
@@ -14,27 +14,25 @@ class CNN(object):
 		# define weights
 		self.dense_w = {}
 		self.pool_w = {}
-		self.conv_w {}
+		self.conv_w  = {}
 
 		# initiate weights
 		self.initiate_weights()
 
 
-
-
 	def initiate_weights(self):
 		
-
 		# initiating dense weights
-		for i in range(1, num_dense):
-			self.dense_w["w" + str(i)] = np.random.rand(self.dense_output+1, self.dense_output+1)
+		for i in range(1, self.num_dense):
+			self.dense_w["w" + str(i)] = torch.rand(self.dense_output+1, self.dense_output+1)
 
-		self.dense_w["w" + str(self.)] = np.random.rand(self.dense_output, self.dense_output+1)
+		self.dense_w["w" + str(self.num_dense)] = torch.rand(self.dense_output, self.dense_output+1)
 
 
-
-	def predict(self):
-		pass
+	def predict(self, x, y):
+		
+		for i in range(self.num_convs):
+			pass
 
 	def train(self):
 		pass
@@ -48,8 +46,10 @@ class CNN(object):
 	def single_pool(self):
 		pass
 
-	def pad(self):
-		pass
+	def pad(self, a, pad_num = 1):
+		padding = [pad_num, pad_num, pad_num, pad_num]
+		return torch.nn.functional.pad(a, pad=padding)
+
 
 	def sigmoid(self):
 		pass
@@ -65,3 +65,4 @@ class CNN(object):
 
 	def dense_forward(self):
 		pass
+
