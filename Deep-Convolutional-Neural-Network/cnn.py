4558a971f5a9ce44a4918d8e4d827f0409205a84
Gregory Eales <gregoeales@aol.com>
2019-02-28 15:01:44 -0600

    Update cnn.py
    completed pool forward

--- Deep-Convolutional-Neural-Network/cnn.py
+++ Deep-Convolutional-Neural-Network/cnn.py
@@ -72,33 +72,55 @@ class CNN(object):
 		self.conv_a[a+str(conv_layer)] = torch.zeros(h_num, w_num, t_num)
 
 		for h in range(h_num):
-
 			for w in range(w_num):
-
 				for t in range(t_num):
 
-					x_slice = x.narrow(0, h*(w_h+step), w_h).narrow(1, h*(w_w+step), w_w).narrow(2, h*(w_t+step), w_t)
+					x_slice = x.narrow(0, h*(w_h+step-1), w_h).narrow(1, w*(w_w+step-1), w_w).narrow(2, t*(w_t+step-1), w_t)
+					self.conv_a[a+str(conv_layer)][h, w, t] = self.single_conv(x_slice, conv_layer=1, activation="tanh")
 
-					self.conv_a[a+str(conv_layer)][ h, w, t] = self.single_conv(x_slice, conv_layer=1, activation="tanh")
+	def single_pool(self, x, pool_type="average"):
 
-					
+		if pool_type == "average":
+			return torch.sum(x)/x.numel()
 
+		if pool_type == "max":
+			return x.max()
 
-	def single_average_pool(self, x):
-		return torch.sum(x)/x.numel()
+	def pool_forward(self, x, pool_layer=1, output_shape=[1, 1, 1], step=1, pool_type="average"):
+		
+		# height, width, thickness
+		w = "w" + str(conv_layer)
+		x_h, x_w, x_t = x.shape[0], x.shape[1], x.shape[2]
+		h_num, w_num, t_num = output_shape[0], output_shape[1], output_shape[2]
+
+		w_h = self.calc_window_side(x_h, h_num)
+		w_w = self.calc_window_side(x_w, w_num)
+		w_t = self.calc_window_side(w_t, t_num)
+
+		self.pool_a[a+str(pool_layer)] = torch.zeros(h_num, w_num, t_num)
+
+		for h in range(h_num):
+			for w in range(w_num):
+				for t in range(t_num):
+
+					x_slice = x.narrow(0, h*(w_h+step-1), w_h).narrow(1, w*(w_w+step-1), w_w).narrow(2, t*(w_t+step-1), w_t)
+					self.conv_a[a+str(conv_layer)][h, w, t] = self.single_pool(x, pool_type)
+
 
 	def pad(self, a, pad_num = 1):
 		padding = [pad_num, pad_num, pad_num, pad_num]
 		return torch.nn.functional.pad(a, pad=padding)
 
-
 	def calc_num_steps(self, step, x, w):
 		return (x-w)/(step) + 1
 
+	def calc_window_side(step, x, length):
+		return  x - ( (length - 1) * step )
 
 	
 	def sigmoid_prime(self, z):
 		return torch.sigmoid(z) * (1-torch.sigmoid(z))
+
 
 	def tanh_prime(self, z):
 		return 1 - torch.square(torch.tanh(z))
