ef8a1b7b2176b7be7e262ad405336c14e2885320
Gregory Eales <gregoeales@aol.com>
2019-02-28 01:15:30 -0600

    Update cnn.py

--- Deep-Convolutional-Neural-Network/cnn.py
+++ Deep-Convolutional-Neural-Network/cnn.py
@@ -1,1 +1,44 @@
+import numpy as np
+import pytorch as pt
 
+
+class CNN(object):
+
+	def __init__(self):
+		pass
+
+	def initiate_weights(self):
+		pass
+
+	def predict(self):
+		pass
+
+	def train(self):
+		pass
+
+	def single_conv(self):
+		pass
+
+	def conv_forward(self):
+		pass
+
+	def single_pool(self):
+		pass
+
+	def pad(self):
+		pass
+
+	def sigmoid(self):
+		pass
+
+	def sigmoid_prime(self):
+		pass
+
+	def tanh(self):
+		pass
+
+	def tanh_prime(self):
+		pass
+
+	def dense_forward(self):
+		pass
