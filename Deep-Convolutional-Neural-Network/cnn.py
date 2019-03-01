e34fab9cc203c94783f4b140d6c3538e44437f19
Gregory Eales <gregoeales@aol.com>
2019-02-28 18:15:08 -0600

    updated conv forward

--- Deep-Convolutional-Neural-Network/cnn.py
+++ Deep-Convolutional-Neural-Network/cnn.py
@@ -7,31 +7,35 @@ class CNN(object):
 	# whats the best way to input network topology
 
 
-	def __init__(self, num_convs, num_dense, dense_output):
+	def __init__(self, num_convs, num_dense, output_shape=1, input_shape = [28, 28, 1]):
 		
 		# define network topology
 		self.num_convs = num_convs
 		self.num_dense = num_dense
-		self.dense_output = dense_output
+		self.output_shape = output_shape
+		self.input_shape = input_shape
 
 		# define weights
 		self.dense_w = {}
 		self.conv_w  = {}
 		self.dense_b = {}
 		self.conv_b  = {}
+		self.pool_a = {}
+		self.conv_a = {}
+		self.dense_a = {}
 
 		# initiate weights
 		self.initiate_weights()
-
 
 	def initiate_weights(self):
 		
 		# initiating dense weights
 		for i in range(1, self.num_dense):
-			self.dense_w["w" + str(i)] = torch.rand(self.dense_output+1, self.dense_output+1)
+			self.dense_w["w" + str(i)] = torch.rand(self.output_shape+1, self.output_shape+1)
 
-		self.dense_w["w" + str(self.num_dense)] = torch.rand(self.dense_output, self.dense_output+1)
+		self.dense_w["w" + str(self.num_dense)] = torch.rand(self.output_shape, self.output_shape+1)
 
+		# initiate convolutional weights
 
 	def predict(self, x, y):
 		
@@ -50,10 +54,10 @@ class CNN(object):
 		b = "b" + str(conv_layer)
 		
 		if activation == "tanh":
-			return torch.tanh(torch.sum(x*self.conv_w[w]) + self.conv_b[b])
+			return torch.tanh(torch.sum(x*self.conv_w[w]))
 
 		if activation == "sigmoid":
-			return torch.sigmoid(torch.sum(x*self.conv_w[w]) + self.conv_b[b])
+			return torch.sigmoid(torch.sum(x*self.conv_w[w]))
 
 
 	def conv_forward(self, x, conv_layer=1, step=1, activation="tanh"):
@@ -63,20 +67,22 @@ class CNN(object):
 		# height, width, thickness
 		w = "w" + str(conv_layer)
 		x_h, x_w, x_t = x.shape[0], x.shape[1], x.shape[2]
-		w_h, w_w, w_t = self.conv_w[w].shape[0], self.conv_w[w].shape[0], self.conv_w[w].shape[0], 
+		w_h, w_w, w_t, w_f = self.conv_w[w].shape[0], self.conv_w[w].shape[1], self.conv_w[w].shape[2], self.conv_w[w].shape[3]
 
 		h_num = self.calc_num_steps(step, x_h, w_h)
 		w_num = self.calc_num_steps(step, x_w, w_w)
 		t_num = self.calc_num_steps(step, x_t, w_t)
 
-		self.conv_a[a+str(conv_layer)] = torch.zeros(h_num, w_num, t_num)
+		print(h_num, w_num, t_num, w_f)
+
+		self.conv_a["a"+str(conv_layer)] = torch.ones(h_num, w_num, t_num, w_f)
 
 		for h in range(h_num):
 			for w in range(w_num):
 				for t in range(t_num):
-
-					x_slice = x.narrow(0, h*(w_h+step-1), w_h).narrow(1, w*(w_w+step-1), w_w).narrow(2, t*(w_t+step-1), w_t)
-					self.conv_a[a+str(conv_layer)][h, w, t] = self.single_conv(x_slice, conv_layer=1, activation="tanh")
+					for f in range(w_f):
+						x_slice = x.narrow(0, h*(w_h+step-1), w_h).narrow(1, w*(w_w+step-1), w_w).narrow(2, t*(w_t+step-1), w_t)
+						self.conv_a["a"+str(conv_layer)][h, w, t, f] = self.single_conv(x_slice, conv_layer=1, activation="tanh")
 
 	def single_pool(self, x, pool_type="average"):
 
@@ -90,41 +96,58 @@ class CNN(object):
 		
 		# height, width, thickness
 		w = "w" + str(conv_layer)
-		x_h, x_w, x_t = x.shape[0], x.shape[1], x.shape[2]
+		x_h, x_w, x_t, x_f = x.shape[0], x.shape[1], x.shape[2], x.shape[3]
 		h_num, w_num, t_num = output_shape[0], output_shape[1], output_shape[2]
 
 		w_h = self.calc_window_side(x_h, h_num)
 		w_w = self.calc_window_side(x_w, w_num)
 		w_t = self.calc_window_side(w_t, t_num)
 
-		self.pool_a[a+str(pool_layer)] = torch.zeros(h_num, w_num, t_num)
+		self.pool_a["a"+str(pool_layer)] = torch.zeros(h_num, w_num, t_num, x_f)
 
 		for h in range(h_num):
 			for w in range(w_num):
 				for t in range(t_num):
 
 					x_slice = x.narrow(0, h*(w_h+step-1), w_h).narrow(1, w*(w_w+step-1), w_w).narrow(2, t*(w_t+step-1), w_t)
-					self.conv_a[a+str(conv_layer)][h, w, t] = self.single_pool(x, pool_type)
-
+					self.conv_a["a"+str(conv_layer)][h, w, t, f] = self.single_pool(x, pool_type)
 
 	def pad(self, a, pad_num = 1):
 		padding = [pad_num, pad_num, pad_num, pad_num]
 		return torch.nn.functional.pad(a, pad=padding)
 
 	def calc_num_steps(self, step, x, w):
-		return (x-w)/(step) + 1
+		return int((x-w)/(step))+ 1
 
 	def calc_window_side(step, x, length):
-		return  x - ( (length - 1) * step )
-
+		return  int(x - ( (length - 1) * step ))
 	
 	def sigmoid_prime(self, z):
 		return torch.sigmoid(z) * (1-torch.sigmoid(z))
 
-
 	def tanh_prime(self, z):
 		return 1 - torch.square(torch.tanh(z))
 
-	def dense_forward(self):
+	def dense_forward(self, x):
 		pass
+
+cnn = CNN(4, 4)
+cnn.conv_w["w1"] = torch.ones([4, 4, 1, 10])
+
+x = torch.ones(28, 28, 1)
+
+cnn.conv_forward(x)
+
+print(cnn.conv_a["a1"][0][0])
+
+
+
+
+
+
+
+
+
+
+
 
