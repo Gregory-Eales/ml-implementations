9116b521ec3bb8cc5a379501ff1522b7b2cd0e8b
Gregory Eales <gregoeales@aol.com>
2019-04-25 18:59:34 -0700

    Update cnn.py

--- Deep-Convolutional-Neural-Network/cnn.py
+++ Deep-Convolutional-Neural-Network/cnn.py
@@ -1,1 +1,45 @@
+import torch as tf
 
+class CNN(object):
+
+    # TO DO:
+    # - initalize dense weights
+    # - initialize conv weights
+    # - initalize pool weights lol :)
+    # - single conv
+    # - single pool
+    # - conv forward
+    # - dense forward
+    # - pool forward
+    # - conv backward
+    # - dense backward
+
+    def __init__(self):
+        pass
+
+    def initialize_dense_weights(self):
+        pass
+
+    def initialize_conv_weights(self):
+        pass
+
+    def single_conv(self):
+        pass
+
+    def single_avg_pool(self):
+        pass
+
+    def conv_forward(self):
+        pass
+
+    def pool_forward(self):
+        pass
+
+    def dense_forward(self):
+        pass
+
+    def conv_backward(self):
+        pass
+
+    def dense_backward(self):
+        pass
