f0d8a8218dd3a74c9b495d86dc17753741fe8ea1
Gregory Eales <gregoeales@aol.com>
2019-04-27 09:34:12 -0700

    added RNN

---
+++ Reccurent-Neural-Networks/RNN.py
@@ -0,0 +1,18 @@
+import torch
+
+class RNN(object):
+
+    def __init__(self, x_shape, y_shape):
+        pass
+
+    def initialize_weights(self):
+        pass
+
+    def init_reccurent_weights(self):
+        pass
+
+    def init_dense_weights(self):
+        pass
+
+    def reccurent_forward(self):
+        pass
