ebabcf3bf6b2d6c27e02e09a83d07e9bb88e5c5b
Gregory Eales <gregoeales@aol.com>
2019-08-12 09:32:05 -0700

    added weight initialization

--- Deep-Neural-Networks/main.py
+++ Deep-Neural-Networks/main.py
@@ -1,5 +1,10 @@
-from neural_network import NeuralNetwork 
+from neural_network import NeuralNetwork
 import time
 
 
 
+
+
+NN = NeuralNetwork(10, 10, 10)
+
+NN.initialize_weights()
--- Deep-Neural-Networks/neural_network.py
+++ Deep-Neural-Networks/neural_network.py
@@ -1,47 +1,94 @@
-import numpy
+import numpy as np
 import torch
-
-
-
 
 # neural network class
 class NeuralNetwork(object):
 
-	def __init__(self, input_shape, output_shape, num_layers):
-		
+	def __init__(self, input_shape, output_shape, num_layers, gpu=False):
 
 		# internalize network parameters
+		self.gpu = gpu
 		self.input_shape = input_shape
 		self.output_shape = output_shape
 		self.num_layers = num_layers
 
+		# create variables
+		self.w = None
+		self.b = None
+		self.z = None
+		self.a = None
+
+
 		# initialize weights
-		self.w = self.initialize_weights()
+		self.initialize_weights()
 
 		# initialize bias cache
-		self.b = self.initialize_bias()
+		self.initialize_bias()
 
 		# initialize activation caches
-		self.a = self.initialize_activations()
-		pass
+		self.initialize_activations()
 
 		# initialize z caches
-		self.z = self.initialize_z()
+		self.initialize_z()
 
 	def initialize_weights(self):
-		pass
+
+		self.w = {}
+
+		for i in range(self.num_layers):
+
+			# use pytorch if gpu is true
+			if self.gpu == True:
+				self.w["w" + str(i)] = torch.randn(5, 7, dtype=torch.float16)
+
+
+			# use numpy if gpu is false
+			if self.gpu == False:
+				self.w["w" + str(i)] = np.random.random(size=None)
 
 	def initialize_bias(self):
-		pass
+
+		self.b = {}
+
+		for i in range(self.num_layers):
+
+			# use pytorch if gpu is true
+			if self.gpu == True:
+				self.b["b" + str(i)] = torch.randn(5, 7, dtype=torch.float16)
+
+
+			# use numpy if gpu is false
+			if self.gpu == False:
+				self.b["b" + str(i)] = np.random.random(size=None)
+
+
 
 	def initialize_activations(self):
-		pass
+		self.a = {}
+
+		for i in range(self.num_layers):
+
+			# use pytorch if gpu is true
+			if self.gpu == True:
+				self.a["a" + str(i)] = torch.randn(5, 7, dtype=torch.float16)
+
+
+			# use numpy if gpu is false
+			if self.gpu == False:
+				self.a["a" + str(i)] = np.random.random(size=None)
+
+
 
 	def initialize_z(self):
-		pass
 
+		self.z = {}
 
+		for i in range(self.num_layers):
 
+			# use pytorch if gpu is true
+			if self.gpu == True:
+				self.z["z" + str(i)] = torch.randn(5, 7, dtype=torch.float16)
 
-
-
+			# use numpy if gpu is false
+			if self.gpu == False:
+				self.w["w" + str(i)] = np.random.random(size=None)
