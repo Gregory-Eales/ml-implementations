5b4b5a914c8d1056cde5890022427b2311282e5b
Gregory Eales <gregoeales@aol.com>
2019-08-15 10:26:12 -0700

    Update neural_network.py

--- Deep-Neural-Networks/neural_network.py
+++ Deep-Neural-Networks/neural_network.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 # neural network class
 class NeuralNetwork(object):
 
-	def __init__(self, input_shape=1, output_shape=1, num_layers=3, gpu=False):
+	def __init__(self, input_shape=1, output_shape=1, num_layers=3, gpu=True):
 
 		# check for correct input types and values
 		assert type(input_shape) == int, "input shape needs to be an integer"
@@ -66,20 +66,6 @@ class NeuralNetwork(object):
 				else:
 					self.w["w" + str(i)] = torch.randn(self.input_shape+1, self.input_shape+1, dtype=torch.float32)
 
-			# use numpy if gpu is false
-			if self.gpu == False:
-
-				if (self.num_layers-1) == i:
-					self.w["w" + str(i)] = np.random.random([self.input_shape+1, self.output_shape])
-					self.w["w" + str(i)] = self.w["w" + str(i)].astype('float16')
-
-				elif (self.num_layers-1) != 1:
-					self.w["w" + str(i)] = np.random.random([self.input_shape, self.input_shape+1])
-					self.w["w" + str(i)] = self.w["w" + str(i)].astype('float16')
-
-				else:
-					self.w["w" + str(i)] = np.random.random([self.input_shape+1, self.input_shape+1])
-					self.w["w" + str(i)] = self.w["w" + str(i)].astype('float16')
 
 	def initialize_bias(self):
 
@@ -91,22 +77,12 @@ class NeuralNetwork(object):
 			if self.gpu == True:
 
 				if (self.num_layers-1) == i:
-					self.b["b" + str(i)] = torch.randn(self.output_shape, dtype=torch.float32)
+					self.b["b" + str(i)] = torch.zeros(self.output_shape, dtype=torch.float32)
 
 				else:
-					self.b["b" + str(i)] = torch.randn(self.input_shape+1, dtype=torch.float32)
-
-			# use numpy if gpu is false
-			elif self.gpu == False:
-
-				if (self.num_layers-1) == i:
-					self.b["b" + str(i)] = np.random.random([1, self.output_shape])
-					self.b["b" + str(i)] = self.b["b" + str(i)].astype('float16')
+					self.b["b" + str(i)] = torch.zeros(self.input_shape+1, dtype=torch.float32)
 
 
-				else:
-					self.b["b" + str(i)] = np.random.random([1, self.input_shape+1])
-					self.b["b" + str(i)] = self.b["b" + str(i)].astype('float16')
 
 	def initialize_activations(self):
 
@@ -118,9 +94,6 @@ class NeuralNetwork(object):
 			if self.gpu == True:
 				self.a["a" + str(i)] = torch.randn(1, dtype=torch.float32)
 
-			# use numpy if gpu is false
-			if self.gpu == False:
-				self.a["a" + str(i)] = np.random.random(size=None)
 
 	def initialize_z(self):
 
@@ -132,9 +105,6 @@ class NeuralNetwork(object):
 			if self.gpu == True:
 				self.z["z" + str(i)] = torch.randn(1, dtype=torch.float32)
 
-			# use numpy if gpu is false
-			if self.gpu == False:
-				self.w["w" + str(i)] = np.random.random(size=None)
 
 	######################
 	# Activation Methods #
@@ -186,27 +156,15 @@ class NeuralNetwork(object):
 				self.z["z" + str(i)] = torch.mm(self.a["a"+str(i-1)], self.w["w"+str(i)]) + self.b["b" + str(i)]
 				self.a["a" + str(i)] = self.tanh_torch(self.z["z" + str(i)])
 
-			print("a: ", self.a["a"+str(last_layer-1)].shape)
-			print("w: ", self.w["w"+str(last_layer)].shape)
-			print("b: ", self.b["b" + str(last_layer)].shape)
 
 			self.z["z" + str(last_layer)] = torch.mm(self.a["a"+str(last_layer-1)], self.w["w"+str(last_layer)]) + self.b["b" + str(last_layer)]
 			self.a["a" + str(last_layer)] = self.sigmoid_torch(self.z["z" + str(last_layer)])
 
-		if self.gpu == False:
-
-			for i in range(1, self.num_layers-1):
-				self.z["z" + str(i)] = np.sum(self.a["a"+str(i-1)] * self.w["w"+str(i)], axis=1) + self.b["b" + str(i)]
-				self.a["a" + str(i)] = self.tanh_np(self.z["z" + str(i)])
-
-			self.z["z" + str(last_layer)] = np.sum(self.a["a"+str(last_layer-1)] * self.w["w"+str(last_layer)], axis=1) + self.b["b" + str(last_layer)]
-			self.a["a" + str(last_layer)] = self.sigmoid_np(self.z["z" + str(last_layer)])
-
 	def cost(self, y_hat, y):
-		return 0.5*(y-y_hat)**2
+		return torch.sum(0.5*(y_hat - y)**2)
 
 	def cost_prime(self, y_hat, y):
-		return (y-y_hat)
+		return (y_hat - y)
 
 	def calculate_updates(self, y):
 		cost_prime = self.cost(self.a["a"+str(self.num_layers-1)], y)
@@ -217,51 +175,34 @@ class NeuralNetwork(object):
 				if i != (self.num_layers-1):
 					w_curr = self.w["w"+str(i)]
 					w_prev = self.w["w"+str(i+1)]
-					a_prime = torch.reshape(torch.sum( self.tanh_prime_torch(self.z["z"+str(i)]), dim=0), [-1, 1])
-					a_ahead = torch.reshape(torch.sum(self.a["a"+str(i-1)], dim=0), [-1, 1])
-					#a_ahead = self.a["a"+str(i-1)]
+					#a_prime = torch.reshape(torch.sum( self.tanh_prime_torch(self.z["z"+str(i)]), dim=0), [-1, 1])
+					a_prime = self.tanh_prime_torch(self.z["z"+str(i)])
+					a_ahead = self.a["a"+str(i-1)]
 					prev_update = self.updates["w" + str(i+1)]
-					if True:
-						print("This is it boys")
-						print("####################")
-						print("prev update: ", prev_update.shape)
-						print("a prime: ", a_prime.shape)
-						print("a ahead: ", a_ahead.shape)
-						print("w prev: ", w_prev.shape)
-						print("w current: ", w_curr.shape)
-						print("#####################")
 
 
-					self.updates["w"+str(i)] = torch.mm(prev_update, torch.t(w_prev))*a_prime
+					self.updates["w"+str(i)] = torch.mm(torch.t(a_ahead), torch.mm(a_prime, torch.mm(prev_update, torch.t(w_prev))))
 
 				else:
 					self.updates["w"+str(i)] = torch.mm(torch.t(self.a["a"+ str(i-1)]), (cost_prime * self.sigmoid_prime_torch(self.z["z"+str(i)])))
 
 			for i in reversed(range(1, self.num_layers)):
-				self.updates["w"+str(i)] = torch.mm(prev_update, self.updates["w"+str(i)])
+				a_ahead = torch.reshape(torch.sum(self.a["a"+str(i-1)], dim=0), [-1, 1])
+				#a_ahead = self.a["a"+str(i-1)]
+				print("a ahead: ", a_ahead.shape)
+				print("bla", self.updates["w"+str(i)].shape)
+				self.updates["w"+str(i)] = self.updates["w"+str(i)])
 
-		if self.gpu == False:
-			for i in reversed(range(1, self.num_layers)):
 
-				if i != (self.num_layers-1):
-					pass
-
-				else:
-					self.a["a"+str(i)] = None
 
 	def update_weights(self, alpha):
 
 		for i in range(1, self.num_layers):
-			print("#########################")
-			print("layer: ", i)
-			print(self.w["w" + str(i)].shape)
-			print(self.updates["w" + str(i)].shape)
-			print("#########################")
 			self.w["w" + str(i)] -= alpha*self.updates["w" + str(i)]
 
 
 
-	def train(self, x, y, batch_size=10, alpha=0.1, iterations=10):
+	def train(self, x, y, batch_size=10, alpha=0.01, iterations=10):
 
 		self.historical_cost = []
 
@@ -280,26 +221,19 @@ class NeuralNetwork(object):
 				self.update_weights(alpha)
 
 
-		if self.gpu==False:
-			for iter in tqdm(range(iterations)):
 
-				# make prediction
-
-				# calculate error
-				self.historical_cost.append(self.cost())
-				# calculate update
-
-				# update weights
 
 def main():
 
 	# create data
-	x = torch.randn(100, 5)
-	y = torch.randn(100, 2)
+	x = torch.ones(1, 5)/2
+	y = torch.ones(1, 2)/2
 
 	# make prediction
-	NN = NeuralNetwork(5, 2, 4, gpu=True)
-	NN.train(x, y)
+	NN = NeuralNetwork(5, 2, 8, gpu=True)
+	NN.train(x, y, iterations=1000, alpha=0.001)
+	plt.plot(NN.historical_cost)
+	plt.show()
 
 if __name__ == "__main__":
 	main()
