066f6bfa58358035c79b22b21de7a39fb3fbfc9d
Gregory Eales <gregoeales@aol.com>
2019-08-18 01:26:16 -0700

    Update vanilla_policy_gradient.py

--- Vanilla-Policy-Gradient/vanilla_policy_gradient.py
+++ Vanilla-Policy-Gradient/vanilla_policy_gradient.py
@@ -2,15 +2,35 @@ import torch
 
 class VPG(object):
 
-	def __init__(self, observation_size, action_size):
+	def __init__(self, observation_size, action_size, size, gamma=0.99, lam=0.95):
 
-		# initialize buffers
+		######################
+		# initialize buffers #
+		######################
+
+		# buffer of observation inputs
 		self.observation_buffer = torch.zeros(observation_size)
+
+		# buffer of outputed actions
 		self.action_buffer = torch.zeros(action_size)
-		self.advantage_buffer = torch.zeros()
 
-	def train(self):
-		pass
+		# buffer of output action advantages over other actions
+		self.advantage_buffer = torch.zeros(size, dtype=torch.float32)
 
-	def act(self):
+		# buffer of returned rewards
+		self.reward_buffer = torch.zeros(size, dtype=torch.float32)
+
+		# buffer of cumilative reward aka return
+		self.return_buffer = torch.zeros(size, dtype=torch.float32)
+
+		# buffer of expected return in a state with an action
+		self.value_buffer = torch.zeros(size, dtype=torch.float32)
+
+		# buffer of log p values
+		self.logp_buffer = torch.zeros(size, dtype=torch.float32)
+
+		# gamma and lam values
+		self.gamma, self.lam = gamma, lam
+
+	def store_trajectory(self, observation, action, reward, value, logp):
 		pass
