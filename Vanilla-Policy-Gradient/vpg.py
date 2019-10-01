27e91f50529944f48b46dea62188d8734bc50f4b
Gregory Eales <gregoeales@aol.com>
2019-09-30 19:38:49 -0700

    Update vpg.py

--- Vanilla-Policy-Gradient/vpg.py
+++ Vanilla-Policy-Gradient/vpg.py
@@ -1,5 +1,12 @@
 import torch
 
+class ValueNetwork(torch.nn.Module):
+
+	def __init__(self, alpha, input_dims, output_dims):
+
+		# inherit from nn module class
+		super(ValueNetwork, self)__init__()
+
 class PolicyNetwork(torch.nn.Module):
 
 	def __init__(self, alpha, input_dims, output_dims):
@@ -7,9 +14,20 @@ class PolicyNetwork(torch.nn.Module):
 		# inherit from nn module class
 		super(PolicyNetwork, self)__init__()
 
+		# initialize network
+		self.initialize_network()
+
 	# initialize network
 	def initialize_network(self):
-		pass
+
+		# define network components
+		self.batch_norm1 = torch.nn.BatchNorm1d()
+		self.fc1 = torch.nn.Linear(4)
+		self.relu1 = torch.nn.ReLU()
+
+	def predict(x):
+		out = self.batch_norm(x)
+		out = self.fc1(out)
 
 	# define loss function
 	def loss(self):
@@ -25,6 +43,8 @@ class VPG(object):
 
 		# initialize policy network
 		self.policy_network = PolicyNetwork(alpha, input_dims, output_dims)
+
+		self.value_network = ValueNetwork(alpha, input_dims, output_dims)
 
 	def act(self):
 		pass
