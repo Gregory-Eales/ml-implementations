549416fdc65f4a45521fb0b60a4cda4adf706d62
Gregory Eales <gregoeales@aol.com>
2019-09-30 19:24:30 -0700

    Update vpg.py

--- Vanilla-Policy-Gradient/vpg.py
+++ Vanilla-Policy-Gradient/vpg.py
@@ -1,39 +1,30 @@
 import torch
 
-class VPG(object):
+class PolicyNetwork(torch.nn.Module):
 
-	def __init__(self, observation_size, action_size, size, gamma=0.99, lam=0.95):
+	def __init__(self, alpha, input_dims, output_dims):
 
-		# buffer of observation inputs
-		self.observation_buffer = torch.zeros(observation_size)
+		# inherit from nn module class
+		super(PolicyNetwork, self)__init__()
 
-		# buffer of outputed actions
-		self.action_buffer = torch.zeros(action_size)
-
-		# buffer of output action advantages over other actions
-		self.advantage_buffer = torch.zeros(size, dtype=torch.float32)
-
-		# buffer of returned rewards
-		self.reward_buffer = torch.zeros(size, dtype=torch.float32)
-
-		# buffer of cumilative reward aka return
-		self.return_buffer = torch.zeros(size, dtype=torch.float32)
-
-		# buffer of expected return in a state with an action
-		self.value_buffer = torch.zeros(size, dtype=torch.float32)
-
-		# buffer of log p values
-		self.logp_buffer = torch.zeros(size, dtype=torch.float32)
-
-		# gamma and lam values
-		self.gamma, self.lam = gamma, lam
-
-	def store_trajectory(self, observation, action, reward, value, logp):
-
-		# store trajectory
-
-	def finish_path(self, last_val=0):
+	# initialize network
+	def initialize_network(self):
 		pass
 
-	def get_buffer_data(self):
+	# define loss function
+	def loss(self):
+		pass
+
+	# training loop
+	def train(self, x, y, iter):
+		pass
+
+class VPG(object):
+
+	def __init__(self, alpha, input_dims, output_dims):
+
+		# initialize policy network
+		self.policy_network = PolicyNetwork(alpha, input_dims, output_dims)
+
+	def act(self):
 		pass
