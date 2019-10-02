71b1e939f52e2f59e78a3853040dab9277b519fb
Gregory Eales <gregoeales@aol.com>
2019-10-01 21:20:23 -0700

    Polishing networks

--- Vanilla-Policy-Gradient/vpg/policy_network.py
+++ Vanilla-Policy-Gradient/vpg/policy_network.py
@@ -2,54 +2,75 @@ import torch
 
 class PolicyNetwork(torch.nn.Module):
 
-	def __init__(self, alpha, input_dims, output_dims):
+    def __init__(self, alpha, input_dims, output_dims):
 
-		# inherit from nn module class
-		super(PolicyNetwork, self)__init__()
+        self.input_dims = input_dims
+        self.output_dims = output_dims
 
-		# initialize network
-		self.initialize_network()
+        # inherit from nn module class
+        super(PolicyNetwork, self).__init__()
+
+        # initialize_network
+        self.initialize_network()
 
         # define optimizer
-        self.optimizer = torch.optim.SGD(lr=alpha)
+        self.optimizer = torch.optim.SGD(lr=alpha, params=self.parameters())
 
         # define loss
-        self.loss = torch.nn.CrossEntropyLoss()
+        #self.loss = torch.nn.NLLLoss()
 
-	# initialize network
-	def initialize_network(self):
+    def loss(self, prediction, y):
+        loss = -prediction*torch.log(y)
+        loss = torch.sum(loss)/loss.shape[0]
+        return loss
+
+    # initialize network
+    def initialize_network(self):
 
 		# define network components
-		self.batch_norm1 = torch.nn.BatchNorm1d()
-		self.fc1 = torch.nn.Linear(4)
-		self.relu1 = torch.nn.ReLU()
+        self.fc1 = torch.nn.Linear(self.input_dims, 3)
+        self.fc2 = torch.nn.Linear(3, self.output_dims)
+        self.relu = torch.nn.ReLU()
+        self.sigmoid = torch.nn.Sigmoid()
 
-	def predict(x):
-		out = self.batch_norm(x)
-		out = self.fc1(out)
+    def predict(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.sigmoid(out)
+        return out
 
-	# training loop
-	def update(self, x, y, iter):
+    def update(self, x, y, iter):
 
         for i in range(iter):
 
             # zero the parameter gradients
-            optimizer.zero_grad()
+            self.optimizer.zero_grad()
 
             # make prediction
             prediction = self.predict(x)
 
             # calculate loss
             loss = self.loss(prediction, y)
+            print("Iteration: {}".format(i))
+            print("Loss: {}".format(loss))
 
             # optimize
-            loss.backward()
+            if i == 0: loss.backward(retain_graph=True)
+            else: loss.backward(retain_graph=True)
             self.optimizer.step()
 
 
-
 def main():
-    pass
+
+
+    x = torch.ones(100, 2, requires_grad=True)
+    y = torch.ones(100, 1, requires_grad=True)/10
+
+    # define PolicyNetwork
+    pn = PolicyNetwork(alpha=0.1, input_dims=2, output_dims=1)
+
+    pn.update(x, y, 100)
 
 if __name__ == "__main__":
     main()
--- Vanilla-Policy-Gradient/vpg/value_network.py
+++ Vanilla-Policy-Gradient/vpg/value_network.py
@@ -2,52 +2,71 @@ import torch
 
 class ValueNetwork(torch.nn.Module):
 
-	def __init__(self, alpha, input_dims, output_dims):
+    def __init__(self, alpha, input_dims, output_dims):
 
-		# inherit from nn module class
-		super(ValueNetwork, self)__init__()
+        self.input_dims = input_dims
+        self.output_dims = output_dims
 
-        # define optimizer
-        self.optimizer = torch.optim.SGD(lr=alpha)
-
-        # define loss
-        self.loss = torch.nn.MSELoss()
+        # inherit from nn module class
+        super(ValueNetwork, self).__init__()
 
         # initialize_network
         self.initialize_network()
 
+        # define optimizer
+        self.optimizer = torch.optim.SGD(lr=alpha, params=self.parameters())
+
+        # define loss
+        self.loss = torch.nn.MSELoss()
+
+
     # initialize network
-	def initialize_network(self):
+    def initialize_network(self):
 
 		# define network components
-		self.batch_norm1 = torch.nn.BatchNorm1d()
-		self.fc1 = torch.nn.Linear(4)
-		self.relu1 = torch.nn.ReLU()
+        self.batch_norm1 = torch.nn.BatchNorm1d(self.input_dims)
+        self.fc1 = torch.nn.Linear(self.input_dims, 3)
+        self.fc2 = torch.nn.Linear(3, self.output_dims)
+        self.relu1 = torch.nn.ReLU()
+        self.relu2 = torch.nn.ReLU()
 
-	def predict(x):
-		out = self.batch_norm(x)
-		out = self.fc1(out)
+    def predict(self, x):
+        out = self.batch_norm1(x)
+        out = self.fc1(out)
+        out = self.relu1(out)
+        out = self.fc2(out)
+        out = self.relu1(out)
+        return out
 
-	def update(self, x, y, iter):
+    def update(self, x, y, iter):
 
         for i in range(iter):
 
             # zero the parameter gradients
-            optimizer.zero_grad()
+            self.optimizer.zero_grad()
 
             # make prediction
             prediction = self.predict(x)
 
             # calculate loss
             loss = self.loss(prediction, y)
+            print("Iteration: {}".format(i))
+            print("Loss: {}".format(loss))
 
             # optimize
             loss.backward()
             self.optimizer.step()
 
-            
+
 def main():
-    pass
+
+    x = torch.ones(100, 2)
+    y = torch.ones(100, 2)
+
+    # defin value ValueNetwork
+    vn = ValueNetwork(alpha=0.01, input_dims=2, output_dims=2)
+
+    vn.update(x, y, 100)
 
 if __name__ == "__main__":
     main()
