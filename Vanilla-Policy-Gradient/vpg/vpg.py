ccdb5f9fefefeca59157a7dcba9a87f9e7cf3e86
Gregory Eales <gregoeales@aol.com>
2019-10-04 18:23:34 -0700

    Update vpg.py

--- Vanilla-Policy-Gradient/vpg/vpg.py
+++ Vanilla-Policy-Gradient/vpg/vpg.py
@@ -27,48 +27,64 @@ class VPG(object):
 
 	def act(self, s):
 
+		# convert to torch tensor
+		s = torch.tensor(s).reshape(-1, len(s)).float()
+
 		# get policy prob distrabution
-		prediction = self.policy_network(s)
+		prediction = self.policy_network.forward(s)
 
 		# convert to numpy array
-		prediction = prediction.numpy()
+		action = prediction.detach().numpy()[0]
 
+		print(list(range(2)), action)
 		# randomly select move based on distrabution
-		np.random.choice(list(range(2)), p=predictions)
+		action = np.random.choice(list(range(2)), p=action/np.sum(action))
+
+		return action, torch.copy(prediction)
 
 
 	def update(self, iterations):
 		pass
 
-	def playthrough(self, env, n_steps, n_actions, n_observations, render=False):
+	def playthrough(self, env, n_steps, render=False):
 
 		# create episode buffers
-		reward_buffer = torch.zeros(1, n_steps, 1)
-		action_buffer = torch.zeros(1, n_steps, n_actions)
-		observation_buffer = torch.zeros(1, n_steps, n_observations)
+		reward_buffer = []
+		action_buffer = []
+		observation_buffer = []
+
+		# reset environment
+		observation = env.reset()
 
 		for t in range(n_steps):
 			# render env screen
-	        if render: env.render()
+			if render: env.render()
 
-	        # get action
-	        action = self.act()
+	        # get action, and network policy prediction
+			action, prediction = self.act(observation)
 
 	        # get state + reward
-	        observation, reward, done, info = env.step(action)
+			observation, reward, done, info = env.step(action)
+
+			# update cumulative reward
+			cum_reward = t+1
 
 	        # check if episode is terminal
-	        if done:
-	            print("Episode finished after {} timesteps".format(t+1))
-	            break
+			if done:
 
-	def train(self, num_episodes, env):
+				# print time step
+				print("Episode finished after {} timesteps".format(t+1))
+
+				# return values
+				return cum_reward
+
+	def train(self, env, num_episodes, n_steps):
 
 		# for each iteration:
 		for episode in range(num_episodes):
 
 			# playthrough an episode to obtain trajectories T
-			self.playthrough(env)
+			self.playthrough(env, n_steps=n_steps, render=False)
 
 			# compute "rewards-to-go"
 			# ???????????????????
@@ -76,8 +92,24 @@ class VPG(object):
 			# compute advantage estimates
 
 			# estimate policy gradient
+			self.policy_network.update(self.buffer.reward_buffer)
 
 			# compute policy gradient
 
 			# update policy
 		pass
+
+def main():
+
+	import gym
+
+	# initialize environment
+	env = gym.make('CartPole-v0')
+
+	vpg = VPG(alpha=0.01, input_dims=4, output_dims=2)
+
+	for i in range(10):
+		vpg.playthrough(env=env, n_steps=100, n_observations=100, n_actions=100, render=True)
+
+if __name__ == "__main__":
+	main()
