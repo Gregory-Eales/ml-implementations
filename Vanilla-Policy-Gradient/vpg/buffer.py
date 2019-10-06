5ba387c0b31d7a3af66df59da56d3d2c0ecf53c9
Gregory Eales <gregoeales@aol.com>
2019-10-06 14:13:04 -0700

    Update buffer.py

--- Vanilla-Policy-Gradient/vpg/buffer.py
+++ Vanilla-Policy-Gradient/vpg/buffer.py
@@ -13,3 +13,12 @@ class Buffer(object):
 
         # store reward
         self.reward_buffer = []
+
+    def store_obs(self, obs):
+        self.observation_buffer.append(obs)
+
+    def store_reward(self, reward):
+        self.reward_buffer.append(reward)
+
+    def store_actions(self, action):
+        self.action_buffer.append(action)
