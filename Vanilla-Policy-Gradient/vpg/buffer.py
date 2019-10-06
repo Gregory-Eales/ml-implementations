03b7b037634c395137bd59f374cd112d4989e0fe
Gregory Eales <gregoeales@aol.com>
2019-10-06 15:46:15 -0700

    Update buffer.py

--- Vanilla-Policy-Gradient/vpg/buffer.py
+++ Vanilla-Policy-Gradient/vpg/buffer.py
@@ -1,5 +1,5 @@
 import torch
-
+import numpy
 
 class Buffer(object):
 
@@ -28,3 +28,25 @@ class Buffer(object):
 
     def store_advantage(self, adv):
         self.advantage_buffer.append(adv)
+
+    def clear_buffer(self):
+        # store actions
+        self.action_buffer = []
+
+        # store state
+        self.observation_buffer = []
+
+        # store reward
+        self.reward_buffer = []
+
+        # store advantage
+        self.advantage_buffer = []
+
+    def get_tensors(self):
+
+        observations = torch.Tensor(self.observation_buffer[0:-1])
+        actions = torch.cat(self.action_buffer)
+        rewards = torch.Tensor(self.reward_buffer).reshape(-1, 1)
+        advantages = torch.Tensor(self.advantage_buffer)
+
+        return observations, actions, rewards, advantages
