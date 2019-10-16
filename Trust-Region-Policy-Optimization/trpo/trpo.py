3dfb98f55e27814e6b5970304c9a5b000e4257e4
Gregory Eales <gregoeales@aol.com>
2019-10-15 19:42:50 -0700

    Update trpo.py

--- Trust-Region-Policy-Optimization/trpo/trpo.py
+++ Trust-Region-Policy-Optimization/trpo/trpo.py
@@ -15,14 +15,32 @@ class TRPO(object):
         self.policy_network = PolicyNetwork(alpha, input_size=input_size,
          output_size=output_size)
 
+    def calculate_advantage(self, prev_observation, observation):
+
+        v1 = self.value_network(prev_observation)
+
+        v2 = self.value_network(observation)
+
+        return 1 + v2 - v1
+
+    def act(self, observation):
+        pass
+
     def train(self, env, epochs=1000, steps=4000):
 
         for epoch in range(epochs):
 
             observation = env.reset()
+            step = 0
 
             for step in range(steps):
 
-                # get action
+                step += 1
 
-                # take step
+                action, log_prob = self.act()
+
+                observation, reward, done, info = env.step(action)
+
+                if done:
+                    observation = env.reset()
+                    step = 0
