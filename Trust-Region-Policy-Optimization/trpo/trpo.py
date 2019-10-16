ed6b1ded663dd9cc3ad3fde04501a25fbd2a7dc9
Gregory Eales <gregoeales@aol.com>
2019-10-15 23:25:48 -0700

    Update trpo.py

--- Trust-Region-Policy-Optimization/trpo/trpo.py
+++ Trust-Region-Policy-Optimization/trpo/trpo.py
@@ -27,7 +27,7 @@ class TRPO(object):
         return 1 + v2 - v1
 
     def act(self, observation):
-        prediction = self.policy_network.forward(s)
+        prediction = self.policy_network.forward(observation)
         action_probabilities = torch.distributions.Categorical(prediction)
         action = action_probabilities.sample()
         log_prob = action_probabilities.log_prob(action)
@@ -38,28 +38,41 @@ class TRPO(object):
         for epoch in range(epochs):
 
             observation = env.reset()
+            self.buffer.store_observation(observation)
+
             step = 0
 
             for step in range(steps):
 
                 step += 1
 
-                action, log_prob = self.act()
+                action, log_prob = self.act(observation)
+                self.buffer.store_action(log_prob)
 
                 observation, reward, done, info = env.step(action)
+                self.buffer.store_observation(observation)
+                self.buffer.store_reward(reward)
 
-                if done:
+                advantage = self.calculate_advantage()
+                self.buffer.store_advantage(advantage)
+
+                if done or t == n_steps-1:
                     observation = env.reset()
+
+                    for s in reversed(range(1, step+1)):
+                        update = 0
+                        for k in reversed(range(1, s+1)):
+                            update += self.buffer.reward_buffer[-k]*(0.99**k)
+                        self.buffer.reward_buffer[-s] += update
                     step = 0
-
 def main():
-
     import gym
     torch.manual_seed(1)
     np.random.seed(1)
     env = gym.make('MountainCar-v0')
-    observation = env.reset()
-    print(type(observation.tolist()))
+
+    trpo = TRPO(alpha=0.001, input_size=2, output_size=3)
+    trpo.train(env, epochs=1, steps=200)
 
 if __name__ == "__main__":
     main()
