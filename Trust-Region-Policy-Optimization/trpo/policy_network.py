24044d4d7a1585b54f06ae795ba67c2e71769c7f
Gregory Eales <gregoeales@aol.com>
2019-10-15 23:25:45 -0700

    Update policy_network.py

--- Trust-Region-Policy-Optimization/trpo/policy_network.py
+++ Trust-Region-Policy-Optimization/trpo/policy_network.py
@@ -2,9 +2,11 @@ import torch
 import numpy as np
 
 
-class PolicyNetwork(object):
+class PolicyNetwork(torch.nn.Module):
 
     def __init__(self, alpha, input_size, output_size):
+
+        super(PolicyNetwork, self).__init__()
 
         self.input_size = input_size
         self.output_size = output_size
@@ -14,8 +16,9 @@ class PolicyNetwork(object):
         self.fc3 = torch.nn.Linear(128, output_size)
         self.tanh = torch.nn.Tanh()
         self.relu = torch.nn.LeakyReLU()
+        self.sigmoid = torch.nn.Sigmoid()
 
-        self.optimizer = torch.optim.Adam(lr=alpha)
+        self.optimizer = torch.optim.Adam(lr=alpha, params=self.parameters())
 
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu:0')
         self.to(self.device)
@@ -24,13 +27,13 @@ class PolicyNetwork(object):
         pass
 
     def forward(self, x):
-        x = x.to(self.device)
+        x = torch.Tensor(x).to(self.device)
         out = self.fc1(x)
         out = self.tanh(out)
         out = self.fc2(out)
         out = self.tanh(out)
         out = self.fc3(out)
-        out = self.relu(out)
+        out = self.sigmoid(out)
         out = out.to(torch.device('cpu:0'))
 
     def optimize(self, iterations=1):
