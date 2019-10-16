1092d0cf9f4b387bef252537f35729b9a5d6e18b
Gregory Eales <gregoeales@aol.com>
2019-10-15 20:30:56 -0700

    Update trpo.py

--- Trust-Region-Policy-Optimization/trpo/trpo.py
+++ Trust-Region-Policy-Optimization/trpo/trpo.py
@@ -1,13 +1,16 @@
 import torch
-import numpy
+import numpy as np
 
 from policy_network import PolicyNetwork
 from value_network import ValueNetwork
+from buffer import Buffer
 
 
 class TRPO(object):
 
     def __init__(self, alpha, input_size, output_size):
+
+        self.buffer = Buffer()
 
         self.value_network = ValueNetwork(alpha, input_size=input_size,
          output_size=1)
@@ -24,7 +27,11 @@ class TRPO(object):
         return 1 + v2 - v1
 
     def act(self, observation):
-        pass
+        prediction = self.policy_network.forward(s)
+        action_probabilities = torch.distributions.Categorical(prediction)
+        action = action_probabilities.sample()
+        log_prob = action_probabilities.log_prob(action)
+        return action.item(), log_prob
 
     def train(self, env, epochs=1000, steps=4000):
 
@@ -44,3 +51,15 @@ class TRPO(object):
                 if done:
                     observation = env.reset()
                     step = 0
+
+def main():
+
+    import gym
+    torch.manual_seed(1)
+    np.random.seed(1)
+    env = gym.make('MountainCar-v0')
+    observation = env.reset()
+    print(type(observation.tolist()))
+
+if __name__ == "__main__":
+    main()
