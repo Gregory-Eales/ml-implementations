90a77108b0a0482badf2d55a12907d649f17e266
Gregory Eales <gregoeales@aol.com>
2019-10-15 20:30:46 -0700

    Update buffer.py

--- Trust-Region-Policy-Optimization/trpo/buffer.py
+++ Trust-Region-Policy-Optimization/trpo/buffer.py
@@ -1,20 +1,38 @@
 import torch
-import numpy
-
+import numpy as np
 
 class Buffer(object):
 
     def __init__(self):
-        pass
 
-    def store_reward(self):
-        pass
+        self.observation_buffer = []
+        self.action_buffer = []
+        self.reward_buffer = []
+        self.advantage_buffer = []
 
-    def store_observation(self):
-        pass
+    def store_reward(self, r):
+        r = torch.tensor(r)
+        self.reward_buffer.append(r)
 
-    def store_advantage(self):
-        pass
+    def store_observation(self, s):
+        s = torch.from_numpy(s)
+        self.observation_buffer.append(s)
 
-    def store_action(self):
-        pass
+    def store_advantage(self, a):
+        self.advantage_buffer.append(a)
+
+    def store_action(self, a):
+        self.action_buffer.append(a)
+
+    def clear_buffer(self):
+        self.observation_buffer = []
+        self.action_buffer = []
+        self.reward_buffer = []
+        self.advantage_buffer = []
+
+    def get_tensors(self):
+        observations = torch.cat(self.observation_buffer)
+        actions = torch.cat(self.action_buffer)
+        rewards = torch.cat(self.reward_buffer)
+        advantages = torch.cat(self.advantage_buffer)
+        return observations, actions, rewards, advantages
