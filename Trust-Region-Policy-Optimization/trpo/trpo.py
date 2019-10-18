0ad3d5a96aa3ca4a7898962d5a6f05e2719b7ad1
Gregory Eales <gregoeales@aol.com>
2019-10-17 17:06:33 -0700

    Update trpo.py

--- Trust-Region-Policy-Optimization/trpo/trpo.py
+++ Trust-Region-Policy-Optimization/trpo/trpo.py
@@ -18,11 +18,27 @@ class TRPO(object):
         self.policy_network = PolicyNetwork(alpha, input_size=input_size,
          output_size=output_size)
 
+        self.old_policy_network = PolicyNetwork(alpha, input_size=input_size,
+         output_size=output_size)
+
+        # store policy state
+        self.buffer.store_parameters(self.policy_network.state_dict())
+
     def update(self, iter=80):
 
-        observations, actions, rewards, advantages = self.buffer.get_tensors()
+        observations = self.buffer.get_observations()
+        actions = self.buffer.get_actions()
+        rewards = self.buffer.get_rewards()
+        advantages = self.buffer.get_advantages()
+        log_probs = self.buffer.get_log_probs()
 
-        self.policy_network.optimize(log_prob, advantage, prev_params, iter=1)
+        self.old_policy_network.load_state_dict(self.buffer.old_parameters)
+        old_probs = self.old_policy_network.forward(observations)
+        self.buffer.store_parameters(self.policy_network.state_dict())
+
+
+        self.policy_network.optimize(log_probs, old_probs,
+         advantages, prev_params, iter=1)
 
         self.value_network.optimize(observations, rewards, iter=iter)
 
@@ -43,6 +59,13 @@ class TRPO(object):
         action = action_probabilities.sample()
         log_prob = action_probabilities.log_prob(action)
         return action.item(), log_prob
+
+    def discount_rewards(self, step):
+        for s in reversed(range(1, step+1)):
+            update = 0
+            for k in reversed(range(1, s+1)):
+                update += self.buffer.reward_buffer[-k]*(0.99**k)
+            self.buffer.reward_buffer[-s] += update
 
     def train(self, env, epochs=1000, steps=4000):
 
@@ -69,12 +92,7 @@ class TRPO(object):
 
                 if done or step == steps-1:
                     observation = env.reset()
-
-                    for s in reversed(range(1, step+1)):
-                        update = 0
-                        for k in reversed(range(1, s+1)):
-                            update += self.buffer.reward_buffer[-k]*(0.99**k)
-                        self.buffer.reward_buffer[-s] += update
+                    self.discount_rewards(step)
                     step = 0
 def main():
     import gym
