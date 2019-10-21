b3d7bcca8c69ea1a2a5ae4a3f6832d80a8e3ece6
Gregory Eales <gregoeales@aol.com>
2019-10-20 17:12:56 -0700

    Added Proximal Policy Optimization

---
+++ Proximal-Policy-Optimization/ppo/ppo.py
@@ -0,0 +1,13 @@
+import torch
+import numpy
+
+class PPO(object):
+
+    def __init__(self):
+        pass
+
+    def act(self):
+        pass
+
+    def train(self):
+        pass
---
+++ Trust-Region-Policy-Optimization/trpo/agent.py
@@ -0,0 +1,13 @@
+from tensorflow.keras.layers import Dense, Activation, Input
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.optimizers import Adam
+import keras.backend as k
+import numpy as np
+
+class Agent(object):
+
+    def __init__(self, alpha, gamma, input_shape, output_shape):
+        pass
+
+
+    
