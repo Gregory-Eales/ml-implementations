c0facc5727fd4d73bb428af00a96775d3e34b620
Gregory Eales <gregoeales@aol.com>
2019-10-26 21:18:40 -0700

    Update linear_regression.py

--- Linear-Regression/linear_regression/linear_regression.py
+++ Linear-Regression/linear_regression/linear_regression.py
@@ -8,17 +8,18 @@ class LinearRegressor(object):
         print(self.w.shape)
         self.b = np.zeros([1, 1])
         self.historical_error = []
+        self.historical_accuracy = []
 
     def mean_squared_error(self, prediction, y):
-        return 0.5*np.sum((prediction - y))**2 / y.shape[0]
+        return 0.5*np.sum((prediction - y)**2) / y.shape[0]
 
     def mse_prime(self, prediction, y):
-        return np.sum(prediction-y, axis=0) / y.shape[0]
+        return prediction-y
 
     def loss(self, x, prediction, y):
         loss = self.mse_prime(prediction, y)
-        delta_w = loss*np.sum(x, axis=0).reshape(2, 1)
-        delta_b = loss
+        delta_w = np.sum(x*loss, axis=0).reshape(2, 1)/x.shape[0]
+        delta_b = np.sum(loss)/x.shape[0]
         return delta_w, delta_b
 
     def predict(self, x):
@@ -28,18 +29,17 @@ class LinearRegressor(object):
 
         for i in range(iter):
             prediction = self.predict(x)
-            prediction[prediction<0.5] = 0
-            prediction[prediction>=0.5] = 1
             error = self.mean_squared_error(prediction, y)
             self.historical_error.append(error)
             delta_w, delta_b = self.loss(x, prediction, y)
             self.w = self.w - alpha*delta_w
             self.b = self.b - alpha*delta_b
+            self.historical_accuracy.append(self.get_accuracy(x, y))
 
     def decision_boundry(self, start, stop, step):
 
-        b = (self.b[0])/-self.w[0]
-        m = -self.w[1]/-self.w[0]
+        b = (self.b[0] - 0.5)/(-self.w[0])
+        m = (self.w[1])/(-self.w[0])
         t = np.arange(start, stop, step)
         s = m*t + b
         return t, s
@@ -50,15 +50,13 @@ class LinearRegressor(object):
         predictions[predictions>=0.5] = 1
         total_samples = y.shape[0]
         actual_pred = y
-        pred_error = np.sum(predictions-y)
-        print("total error:",pred_error)
-        print("total sample:",total_samples)
-        print("accuracy:",(total_samples-pred_error)/total_samples, "%")
+        pred_error = np.sum(np.absolute(predictions-y))
+        return 100*(total_samples-pred_error)/total_samples
 
 
 def main():
-    x = np.random.random([1000, 2])
-    y = np.random.random([1000, 1])
+    x, y = generate_data()
+    x_shuffle, y_shuffle = shuffle(x, y, random_state=0)
     lr = LinearRegressor(2)
     print(lr.w)
     lr.train(x, y, iter=5000, alpha=0.0001)
