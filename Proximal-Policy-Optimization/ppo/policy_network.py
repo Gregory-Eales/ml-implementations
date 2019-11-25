c8046da05fe68e2dc7490c101103f56a87827aae
Gregory Eales <gregoeales@aol.com>
2019-11-24 21:00:14 -0800

    Update policy_network.py

--- Proximal-Policy-Optimization/ppo/policy_network.py
+++ Proximal-Policy-Optimization/ppo/policy_network.py
@@ -9,21 +9,23 @@ class PolicyNetwork(torch.nn.Module):
         self.in_dim = in_dim
         self.out_dim = out_dim
 
-        self.relu = torch.nn.LeakyReLU()
-        self.sigmoid = torch.nn.Sigmoid()
-
-        self.l1 = torch.nn.Linear(in_dim, 128)
-        self.l2 = torch.nn.Linear(128, 128)
-        self.l3 = torch.nn.Linear(128, out_dim)
-
+        self.optimizer = torch.optim.Adam(params=self.params, lr=alpha)
 
         self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu:0')
 
     def define_network(self):
+        self.relu = torch.nn.LeakyReLU()
+        self.sigmoid = torch.nn.Sigmoid()
+        self.l1 = torch.nn.Linear(self.in_dim, 128)
+        self.l2 = torch.nn.Linear(128, 128)
+        self.l3 = torch.nn.Linear(128, self.out_dim)
 
+    def normalize(self):
+        pass
 
     def forward(self, x):
         out = torch.Tensor(x).to(self.device)
+
         out = self.l1(out)
         out = self.relu(out)
         out = self.l2(out)
