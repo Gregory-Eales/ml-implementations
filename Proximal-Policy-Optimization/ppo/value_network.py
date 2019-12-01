25a9fefe3a4fa2ab23e23c106d5ae920f904102d
Gregory Eales <gregoeales@aol.com>
2019-11-30 17:31:36 -0800

    Update value_network.py

--- Proximal-Policy-Optimization/ppo/value_network.py
+++ Proximal-Policy-Optimization/ppo/value_network.py
@@ -9,7 +9,10 @@ class ValueNetwork(torch.nn.Module):
         self.in_dim = in_dim
         self.out_dim = out_dim
 
+        self.define_network()
+
         self.optimizer = torch.optim.Adam(params=self.parameters(), lr=alpha)
+        self.loss = torch.nn.MSELoss()
 
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu:0')
 
@@ -32,3 +35,17 @@ class ValueNetwork(torch.nn.Module):
         out = self.l3(out)
         out = self.sigmoid(out)
         return out.to(torch.device('cpu:0'))
+
+    def update(self):
+        pass
+
+def main():
+
+    t1 = torch.ones(1, 3)
+    vn = ValueNetwork(0.01, 3, 1)
+    print(vn(t1))
+
+
+
+if __name__ == "__main__":
+    main()
