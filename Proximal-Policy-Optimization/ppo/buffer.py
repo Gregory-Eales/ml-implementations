08e199910933402ece0b0a6f82fab4693b93a169
Gregory Eales <gregoeales@aol.com>
2019-11-30 18:27:53 -0800

    Update buffer.py

--- Proximal-Policy-Optimization/ppo/buffer.py
+++ Proximal-Policy-Optimization/ppo/buffer.py
@@ -4,4 +4,35 @@ import numpy as np
 class Buffer(object):
 
     def __init__(self):
+
+        self.rewards = []
+        self.states = []
+        self.actions = []
+        self.discounted_rewards = []
+
+    def store_reward(self, reward):
+        self.rewards.append(reward)
+
+    def store_state(self, state):
+        self.states.append(state)
+
+    def store_action(self, action):
+        self.actions.append(action)
+
+    def discount_rewards(n_steps):
+        pass
+
+    def get_rewards(self):
+        return self.rewards
+
+    def get_states(self):
+        return self.states
+
+    def get_actions(self):
+        return self.actions
+
+    def get_discounted_rewards(self):
+        return self.discount_rewards
+
+    def get_values(self):
         pass
