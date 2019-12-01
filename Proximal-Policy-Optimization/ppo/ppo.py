761d7144fc8f99acd2f2224802a55680c978f49f
Gregory Eales <gregoeales@aol.com>
2019-11-30 18:23:54 -0800

    Update ppo.py

--- Proximal-Policy-Optimization/ppo/ppo.py
+++ Proximal-Policy-Optimization/ppo/ppo.py
@@ -14,21 +14,38 @@ class PPO(object):
         self.value_net = ValueNetwork(alpha, in_dim, out_dim)
         self.policy_net = PolicyNetwork(alpha, in_dim, out_dim)
 
-    def train(self, env, num_steps, iter):
+
+    def store(self, state, action, reward):
+
+        self.buffer.store_state(state)
+        self.buffer.store_action(action)
+        self.buffer.store_reward(reward)
+
+    def calc_discounted_rewards(self, n_steps):
+        # calculates the discounted reward using the past n rewards, where
+        # n is equal to the number of steps
+        self.buffer.discount_reward(n_steps)
+
+    def train(self, env, n_steps, n_epoch, render=False, verbos=False):
 
 
         for i in range(iter):
 
             # reset env
-            env.reset()
+            state = env.reset()
 
             for s in range(nume_steps):
 
+                # render if true
+                if render: env.render()
+
+                # get action
+                action = self.get_action()
+
                 # get observation
+                state, reward, done, info = env.step(action)
 
-                # take action
-
-                # get reward
-                pass
+                # store metrics
+                self.store(state, action, reward)
 
             # update networks
