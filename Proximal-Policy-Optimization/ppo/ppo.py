243f75d6528550a757652ded8bb5a514eb8e7435
Gregory Eales <gregoeales@aol.com>
2019-12-02 14:54:52 -0800

    Update ppo.py

--- Proximal-Policy-Optimization/ppo/ppo.py
+++ Proximal-Policy-Optimization/ppo/ppo.py
@@ -26,6 +26,7 @@ class PPO(object):
         self.buffer.store_state(state)
         self.buffer.store_action(action)
         self.buffer.store_reward(reward)
+        self.calculate_advantages()
 
     def calculate_advantages(self):
 
@@ -39,18 +40,22 @@ class PPO(object):
         self.buffer.store_advantage(a)
 
     def update(self):
-        state, action, reward, disc_reward, advantage = self.buffer.get_data()
+        states = self.buffer.get_states()
+        disc_reward = self.buffer.get_discounted_rewards()
+        old_pred = self.buffer.get_old_predictions()
+        pred = self.buffer.get_predictions()
+        advantages = self.buffer.get_advantages()
 
     def get_action(self):
         state = torch.Tensor(self.buffer.states[-1]).reshape(1, 3)
         prediction = self.policy_net.forward(state)
         old_pred = self.old_policy_net.forward(state)
-        self.buffer.store_action(prediction)
-        self.buffer.store
+        self.buffer.store_prediction(prediction)
+        self.buffer.store_old_prediction(old_pred)
         action_probabilities = torch.distributions.Categorical(prediction)
         action = action_probabilities.sample()
         log_prob = action_probabilities.log_prob(action)
-        self.buffer.log_probs.append(log_prob)
+        self.buffer.store_log_prob(log_prob)
         # convert discrete to continuous2
         action = -2.0 + 0.1*action.float()
 
@@ -80,24 +85,31 @@ class PPO(object):
                 state, reward, done, info = env.step([0])
 
                 # store metrics
-                if not done: self.store(state, action, reward)
+
 
                 if done or s == n_steps-1:
                     self.buffer.store_action(action)
                     self.buffer.store_reward(reward)
-                    self.buffer.discount_rewards(s+2)
+                    self.buffer.discount_rewards(s+1)
+                    self.calculate_advantages()
+
+
+                else: self.store(state, action, reward)
 
             # update networks
             #self.update()
 
 
+            print("###########")
+            print("states  ", len(self.buffer.states))
+            print("actions ", len(self.buffer.actions))
+            print("pred    ", len(self.buffer.predictions))
+            print("old pred", len(self.buffer.old_predictions))
+            print("r       ", len(self.buffer.rewards))
+            print("log prob", len(self.buffer.log_probs))
+            print("disc rwr", len(self.buffer.discounted_rewards))
+            print("advantag", len(self.buffer.advantages))
 
-            print(len(self.buffer.states))
-            print(len(self.buffer.actions))
-            print(len(self.buffer.rewards))
-            print(len(self.buffer.log_probs))
-            print(len(self.buffer.discounted_rewards))
-            print(len(self.buffer.advantages))
 
 
 def main():
