a4805c2a35165397ab6dbfdc210c12f267fc00ef
Gregory Eales <gregoeales@aol.com>
2019-12-04 15:45:38 -0800

    Update policy_network.py

--- Proximal-Policy-Optimization/ppo/policy_network.py
+++ Proximal-Policy-Optimization/ppo/policy_network.py
@@ -17,31 +17,37 @@ class PolicyNetwork(torch.nn.Module):
 
     def define_network(self):
         self.relu = torch.nn.ReLU()
+        self.leaky_relu = torch.nn.LeakyReLU()
         self.sigmoid = torch.nn.Sigmoid()
-        self.l1 = torch.nn.Linear(self.in_dim, 128)
-        self.l2 = torch.nn.Linear(128, 128)
-        self.l3 = torch.nn.Linear(128, self.out_dim)
+        self.l1 = torch.nn.Linear(self.in_dim, 256)
+        self.l2 = torch.nn.Linear(256, 256)
+        self.l3 = torch.nn.Linear(256, self.out_dim)
 
     def normalize(self):
         pass
 
     def loss(self, log_probs, old_log_probs, advantages):
         r_theta = log_probs/old_log_probs
-
+        clipped_r = r_theta.clamp(1.0 - self.epsilon, 1.0 + self.epsilon)
+        return -torch.min(r_theta*advantages, clipped_r).mean()
 
     def forward(self, x):
         out = torch.Tensor(x).to(self.device)
         out = self.l1(out)
-        out = self.relu(out)
+        out = self.leaky_relu(out)
         out = self.l2(out)
-        out = self.relu(out)
+        out = self.leaky_relu(out)
         out = self.l3(out)
         out = self.relu(out)
 
         return out.to(torch.device('cpu:0'))
 
-    def optimize(self, log_probs, old_log_probs, advantages):
-        pass
+    def optimize(self, log_probs, old_log_probs, advantages, iter=10):
+
+        loss = self.loss(log_probs, old_log_probs, advantages)
+        loss.backward(retain_graph=True)
+        self.optimizer.zero_grad()
+        self.optimizer.step()
 
 def main():
 
