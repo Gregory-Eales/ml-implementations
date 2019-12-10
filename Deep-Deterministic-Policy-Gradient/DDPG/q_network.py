9229918f4794e536e05db2763f3141e5987d4acd
Gregory Eales <gregoeales@aol.com>
2019-12-10 11:33:14 -0800

    Create q_network.py

---
+++ Deep-Deterministic-Policy-Gradient/DDPG/q_network.py
@@ -0,0 +1,14 @@
+import torch
+
+class QNetwork(torch.nn.Module):
+
+    def __init__(self):
+
+        super(QNetwork, self).__init__()
+
+
+    def forward(self):
+        pass
+
+    def optimize(self):
+        pass
