82c64734e52b39d8c6f561072697939dc10920de
Gregory Eales <gregoeales@aol.com>
2019-12-11 17:00:09 -0800

    Update tools.py

--- Deep-Fake-Discriminator/DFD/tools.py
+++ Deep-Fake-Discriminator/DFD/tools.py
@@ -1,6 +1,40 @@
 import torch
 import torchvision
+import cv2
+import numpy as np
+
 
 def load_mp4_file(path):
     vframe, aframes, info = torchvision.io.read_video(path)
     return vframe, aframes, info
+
+
+def load_mp4_cv2(path):
+
+    cap = cv2.VideoCapture(path)
+    frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    buf = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
+
+    fc = 0
+    ret = True
+    while (fc < frameCount  and ret):
+        ret, buf[fc] = cap.read()
+        fc += 1
+    cap.release()
+    return buf
+
+
+def main():
+    p1 = "/Users/gregeales/Desktop/Repositories/ML-Reimplementations/"
+    p2 = "Deep-Fake-Discriminator/DFD/sample_vids/vid1.mp4"
+    t = load_mp4_cv2(p1+p2)
+    t = torch.Tensor(t)
+    print(t.shape)
+
+
+
+if __name__ == "__main__":
+    main()
