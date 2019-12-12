a1629515ef2e23fe3624cc9cad68f6670339bf88
Gregory Eales <gregoeales@aol.com>
2019-12-11 17:21:33 -0800

    Update dfd.py

--- Deep-Fake-Discriminator/DFD/dfd.py
+++ Deep-Fake-Discriminator/DFD/dfd.py
@@ -1,8 +1,9 @@
 import torch
+from tools import load_mp4_cv2
 
 class DeepFakeDiscriminator(torch.nn.Module):
 
-    def __init__(self, in_channels, out_channels, kernal_size):
+    def __init__(self, alpha=0.01):
 
         super(DeepFakeDiscriminator, self).__init__()
 
@@ -16,12 +17,12 @@ class DeepFakeDiscriminator(torch.nn.Module):
 
     def initialize_parameters(self):
 
-        self.conv1 = torch.nn.Conv3d(in_channels, 128, 40)
-        self.conv2 = torch.nn.Conv3d(128, 256, 40)
-        self.conv3 = torch.nn.Conv3d(256, 512, 40)
+        self.conv1 = torch.nn.Conv3d(3, 16, [10, 40, 40])
+        self.conv2 = torch.nn.Conv3d(128, 32, [10, 40, 40])
+        self.conv3 = torch.nn.Conv3d(256, 64, [10, 40, 40])
 
-        self.l1 = torch.nn.Linear(in_features=512, out_features=512)
-        self.l2 = torch.nn.Linear(in_features=512, out_features=1)
+        self.l1 = torch.nn.Linear(in_features=64, out_features=64)
+        self.l2 = torch.nn.Linear(in_features=64, out_features=1)
 
         self.sigmoid = torch.nn.Sigmoid()
         self.leaky_relu = torch.nn.LeakyReLU()
@@ -43,5 +44,20 @@ class DeepFakeDiscriminator(torch.nn.Module):
         for i in range(iter):
             pass
 
-main():
+
+def main():
     dfd = DeepFakeDiscriminator()
+    p1 = "/Users/gregeales/Desktop/Repositories/ML-Reimplementations/"
+    p2 = "Deep-Fake-Discriminator/DFD/sample_vids/vid1.mp4"
+    print("Initialized Net")
+    x = load_mp4_cv2(p1+p2)
+    x = torch.Tensor(x)
+    print("Loaded Tensor")
+    x = x.reshape(1, 3, 10, 1080, 1920)
+    print(x.shape)
+    print(dfd.forward(x).shape)
+
+
+
+if __name__ == "__main__":
+    main()
