d31bd787ead25d12cc4a32ef3944366babca4863
Gregory Eales <gregoeales@aol.com>
2020-01-01 14:44:24 -0800

    Update ppo.py

--- Proximal-Policy-Optimization/ppo/ppo.py
+++ Proximal-Policy-Optimization/ppo/ppo.py
@@ -15,7 +15,11 @@ class PPO(object):
 
         self.value_network = ValueNetwork()
         self.policy_network = PolicyNetwork()
+        self.old_policy_network = PolicyNetwork()
+        state_dict = self.policy_network.state_dict()
+        self.old_policy_network.load_state_dict(state_dict)
         self.buffer = Buffer()
+        self.mean_reward()
 
     def act(self, state):
 
@@ -25,35 +29,42 @@ class PPO(object):
     def calculate_advantages(self):
         pass
 
+
     def update(self, epochs=80):
 
-        states, actions, rewards = self.buffer.get()
+        states, policy, old_policy, rewards, advantages = self.buffer.get()
 
-        self.value_network.optimize()
-        self.policy_network.optimize()
+        r = policy/old_policy
+
+        self.policy_network.optimize(advantages, r)
+        self.value_network.optimize(states, rewards)
 
     def train(self, env, n_steps, n_epochs):
 
         for epoch in range(n_epochs):
 
             state = env.reset()
+
+            print("Collecting Trajectories")
             for step in tqdm(range(n_steps)):
 
                 action = self.act(state)
 
-                state, reward, done, _  = env.step(action)
+                state, reward, done, info = env.step(action)
 
                 self.buffer.store(state, action, reward)
 
                 if done:
-                    pass
+
+                    self.calculate_advantages()
 
 
+            mean_reward = self.buffer.get_rewards()
+            mean_reward = torch.sum(mean_reward)/mean_reward.shape[0]
 
-
-
-
-
+            if self.mean_reward[-1] > 150:
+                file_name = "policy_params.pt"
+                torch.save(self.policy_network.state_dict(), file_name)
 
     def play(self):
         pas
