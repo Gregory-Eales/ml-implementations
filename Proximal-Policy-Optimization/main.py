ea203e7f433e9e9e0197ae2fb1c15d6c343f1264
Gregory Eales <gregoeales@aol.com>
2020-03-16 13:14:29 +0000

    Update main.py

--- Proximal-Policy-Optimization/main.py
+++ Proximal-Policy-Optimization/main.py
@@ -1,10 +1,12 @@
 from ppo.ppo import PPO
 import gym
+import torch
+import numpy as np
 
 torch.manual_seed(1)
 np.random.seed(1)
 
 env = gym.make('Pendulum-v0')
-ppo = PPO(alpha=0.001, input_size=2, output_size=3)
+ppo = PPO(alpha=0.001, in_dim=2, out_dim=3)
 
 ppo.train(env=env, epochs=1, steps=1000)
