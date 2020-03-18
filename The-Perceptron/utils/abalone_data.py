636925631d8dc4dd1eedfdd2149a7c2de6655be5
Gregory Eales <gregoeales@aol.com>
2020-03-18 20:11:23 +0000

    Update abalone_data.py

--- The-Perceptron/utils/abalone_data.py
+++ The-Perceptron/utils/abalone_data.py
@@ -6,12 +6,12 @@ import requests
 
 def get_abalone():
 
-	to_predict = "Height"
+	to_predict = "Gender"
 
 	url="https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data"
 	s=requests.get(url).content
 	abalone_dataset = pd.read_csv(io.StringIO(s.decode('utf-8')))
-	abalone_dataset.columns = ['Gender', "Length", "Diameter", "Height", 
+	abalone_dataset.columns = ['Gender', "Length", "Diameter", "Height",
 		"Whole Weight", "Shucked Weight", "Viscera Weight", "Shell Weight",
 		 "Rings"]
 
@@ -21,9 +21,13 @@ def get_abalone():
 	abalone_dataset['Gender'] = np.where(abalone_dataset['Gender'] == "M", 1,
 	 abalone_dataset['Gender'])
 
-	y = np.array(abalone_dataset[to_predict].values)
+	y = np.array(abalone_dataset[to_predict].values).reshape(-1, 1)
+	"""
 	abalone_dataset[to_predict] = np.where(abalone_dataset[to_predict] == 0,
 	 0, abalone_dataset[to_predict])
+	"""
+	abalone_dataset = abalone_dataset.drop(["Gender"], axis=1)
 	x = np.array(abalone_dataset.values)
+	x = np.insert(x,x.shape[1],1,axis=1)
 
 	return x, y
