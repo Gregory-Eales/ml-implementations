c40afabc95ca6492edb81a9a142cb8c243dff209
Gregory Eales <gregoeales@aol.com>
2020-04-20 01:05:25 +0100

    Update buffer.py

--- Deep-Deterministic-Policy-Gradient/DDPG/buffer.py
+++ Deep-Deterministic-Policy-Gradient/DDPG/buffer.py
@@ -4,53 +4,63 @@ import numpy
 
 class Buffer(object):
 
-    def __init__(self):
-        
-        self.reset()
+	def __init__(self):
+		
+		self.reset()
 
-    def reset(self):
-    	self.state_buffer = []
-        self.reward_buffer = []
-        self.discount_reward_buffer = []
-        self.action_buffer = []
-        self.policy_buffer = []
-        self.advantage_buffer = []
+	def reset(self):
+		self.observation_buffer = []
+		self.reward_buffer = []
+		self.discount_reward_buffer = []
+		self.action_buffer = []
+		self.policy_buffer = []
+		self.advantage_buffer = []
+		self.terminal_buffer = []
 
 
-    def store_state(self, s):
-    	pass
+	def store_terminal(self, done):
+		if done: self.terminal_buffer.append(1)
+		else: 0
 
-    def store_reward(self, r):
-    	pass
 
-    def store_disc_reward(self, disc_r):
-    	pass
+	def store(self, observation, reward, done):
+		self.store_observation(observation)
+		self.store_reward(reward)
 
-    def store_action(self, act):
-    	pass
+	def store_observation(self, observation):
+		self.observation_buffer.append(observation)
 
-    def store_policy(self, p):
-    	pass
+	def store_reward(self, r):
+		self.reward_buffer.append(r)
 
-    def store_advantage(self, adv):
-    	pass
+	def store_disc_reward(self, disc_r):
+		self.discount_reward_buffer += disc_r
 
-    def get_state(self, s):
-    	pass
+	def store_action(self, act):
+		self.action_buffer.append(act)
 
-    def get_reward(self, r):
-    	pass
+	def store_policy(self, p):
+		self.policy_buffer.append(p)
 
-    def get_disc_reward(self, disc_r):
-    	pass
+	def store_advantage(self, adv):
+		self.advantage_buffer.append(adv)
 
-    def get_action(self, act):
-    	pass
+	def get_observation(self):
+		return self.observation_buffer
 
-    def get_policy(self, p):
-    	pass
+	def get_reward(self):
+		return self.reward_buffer
 
-    def get_advantage(self, adv):
-    	pass
+	def get_disc_reward(self):
+		return self.discount_reward_buffer
+
+	def get_action(self):
+		return self.action_buffer
+
+	def get_policy(self):
+		return self.policy_buffer
+
+	def get_advantage(self):
+		return self.advantage_buffer
 
 
