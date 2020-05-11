34c516e8ac2cc8da32bfdaccea728498257db99a
Gregory Eales <gregoeales@aol.com>
2020-05-11 22:40:06 +0100

    Update ddpg.py

--- Deep-Deterministic-Policy-Gradient/DDPG/ddpg.py
+++ Deep-Deterministic-Policy-Gradient/DDPG/ddpg.py
@@ -6,11 +6,13 @@ from .q_network import QNetwork
 
 class DDPG(object):
 
-	def __init__(self, in_dim, out_dim):
+	def __init__(self, in_dim, out_dim, p_alpha=1e-10, q_alpha=1e-10):
+
+		self.out_dim = out_dim
 
 		self.buffer = Buffer()
-		self.q_net = QNetwork(in_dim+out_dim, 1)
-		self.p_net = PolicyNetwork(in_dim, out_dim)
+		self.q_net = QNetwork(in_dim+out_dim, 1, alpha=q_alpha)
+		self.p_net = PolicyNetwork(in_dim, out_dim, alpha=p_alpha)
 
 		self.target_q = QNetwork(in_dim+out_dim, 1)
 		self.target_p = PolicyNetwork(in_dim, out_dim)
@@ -21,6 +23,9 @@ class DDPG(object):
 		self.target_p.load_state_dict(p_state_dict)
 		self.target_q.load_state_dict(q_state_dict)
 
+		self.q_loss = []
+		self.p_loss = []
+
 		
 	def compute_targets(self, r, s_p, d, gamma=0.99):
 
@@ -28,7 +33,7 @@ class DDPG(object):
 
 		return r + gamma*(1-d)*self.target_q.forward(s_p, p)
 
-	def update_params(self, p=0.1):
+	def update_params(self, p=0.995):
 		
 		td = self.target_p.state_dict()
 		md = self.p_net.state_dict()
@@ -59,33 +64,40 @@ class DDPG(object):
 		self.buffer.store_disc_reward(disc_reward)
 		
 
-	def act(self, state):
+	def act(self, state, epsilon=0.1):
 		action = self.p_net(state)
-		action = (action + 2*torch.rand(1, 1) - 1).clamp(min=-1, max=1)
-		return action.detach().numpy()
+		rand = epsilon*torch.randn(1, self.out_dim)
+		action = (action + rand).clamp(min=-1, max=1)
+		return action.detach().numpy()[0]
 
-	def update(self, iter=10):
+	def update(self, iter=10, n=100):
 
 		s, a, r, s_p, d = self.buffer.get()
 
 		for i in range(iter):
-			samp_s, samp_a, samp_r, samp_s_p, samp_d = self.buffer.random_sample(s, a, r, s_p, d, n=100)
+			samp_s, samp_a, samp_r, samp_s_p, samp_d = self.buffer.random_sample(s, a, r, s_p, d, n=n)
 
 			y = self.compute_targets(samp_r, samp_s_p, samp_d, gamma=0.99)
 
-			self.update_q_net(samp_s, samp_a, y)
-			self.update_p_net(s)
+			q_loss = self.update_q_net(samp_s, samp_a, y)
+			p_loss = self.update_p_net(samp_s)
+
+			self.q_loss.append(q_loss)
+			self.p_loss.append(p_loss)
 
 			self.update_params()
 		
 
 	def update_q_net(self, s, a, y):
-		self.q_net.optimize(s, a, y)
+		return self.q_net.optimize(s, a, y)
 
 	def update_p_net(self, s):
 		p = self.p_net.forward(s)
 		q = self.q_net.forward(s, p)
-		self.p_net.optimize(q)
+		return self.p_net.optimize(q)
+
+	def clear_buffer(self):
+		self.buffer = Buffer()
 
 
 
