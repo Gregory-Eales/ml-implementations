14f4813181a4b97496cfce6493cf9a9eaab7a787
Gregory Eales <gregoeales@aol.com>
2020-05-11 17:42:56 +0100

    Update policy_network.py

--- Deep-Deterministic-Policy-Gradient/DDPG/policy_network.py
+++ Deep-Deterministic-Policy-Gradient/DDPG/policy_network.py
@@ -2,53 +2,53 @@ import torch
 from torch.nn import functional as F
 from torch import optim
 from torch import nn
-import pytorch_lightning as pl
-from pytorch_lightning import Trainer
 
-class PolicyNetwork(pl.LightningModule):
+class PolicyNetwork(torch.nn.Module):
 
-    def __init__(self, in_dim, out_dim, q_net=None):
+	def __init__(self, in_dim, out_dim, alpha=0.01):
 
-        super(PolicyNetwork, self).__init__()
+		super(PolicyNetwork, self).__init__()
 
-        self.in_dim = in_dim
-        self.out_dim = out_dim
+		self.in_dim = in_dim
+		self.out_dim = out_dim
 
-        self.l1 = nn.Linear(in_dim, 64)
-        self.l2 = nn.Linear(64, 64)
-        self.l3 = nn.Linear(64, out_dim)
+		self.l1 = nn.Linear(in_dim, 64)
+		self.l2 = nn.Linear(64, 64)
+		self.l3 = nn.Linear(64, out_dim)
 
-        self.q_network = q_net
+		self.optimizer = torch.optim.Adam(lr=alpha, params=self.parameters())
 
-    def forward(self, x):
-        
-        out = torch.Tensor(x).reshape(-1, self.in_dim)
-        out = self.l1(out)
-        out = F.relu(out)
-        out = self.l2(out)
-        out = F.relu(out)
-        out = self.l3(out)
-        out = torch.tanh(out)
+		self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu:0')
+		self.to(self.device)
 
-        return out
+	def forward(self, x):
+		
+		out = torch.Tensor(x).reshape(-1, self.in_dim)
+		out = self.l1(out)
+		out = F.relu(out)
+		out = self.l2(out)
+		out = F.relu(out)
+		out = self.l3(out)
+		out = torch.tanh(out)
 
-    def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=1e-3)
-        return optimizer, optim.lr_scheduler.StepLR(optimizer, step_size=1)
+		return out
 
-    def loss(self, output, target):
-        loss = self.loss(output, target)
 
-    def training_step(self, s_batch, batch_idx):
-        actions = self.forward(s_batch)
-        q = self.q_network(s_batch, actions).mean()
-        self.logger.summary.scalar('loss', q)
-        return q
+	def loss(self, q):
+		return torch.mean(q)
+
+	def optimize(self, q):
+
+	  torch.cuda.empty_cache()
+	  self.optimizer.zero_grad()
+	  loss = self.loss(q)
+	  loss.backward(retain_graph=True)
+	  self.optimizer.step()
 
 def main():
-    pn = PolicyNetwork(in_dim=3, out_dim=1)
-    x = torch.ones(10, 3)
-    print(pn.forward(x))
+	pn = PolicyNetwork(in_dim=3, out_dim=1)
+	x = torch.ones(10, 3)
+	print(pn.forward(x))
 
 if __name__ == "__main__":
-    main()
+	main()
