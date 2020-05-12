4b01f8af4a19f65254c3d7ff5d8025be7c7637cd
Gregory Eales <gregoeales@aol.com>
2020-05-12 20:24:33 +0100

    Update main.py

--- Twin-Delayed-DDPG/main.py
+++ Twin-Delayed-DDPG/main.py
@@ -13,6 +13,7 @@ def train(env, ddpg, epochs=10, episodes=200, steps=100, epsilon=0.3,
 	if graph: plt.ion()
 	if render: env.render()
 
+	disc_r = []
 	avg_reward = []
 	for e in tqdm(range(epochs)):
 
@@ -26,14 +27,8 @@ def train(env, ddpg, epochs=10, episodes=200, steps=100, epsilon=0.3,
 			for t in range(steps):
 
 				
-				if e > 50:
-					a = ddpg.act(s, epsilon=epsilon)
-
-				if e == 50:
-					ddpg.clear_buffer()
-
-				else:
-					a = ddpg.act(s, epsilon=0.3)
+				
+				a = ddpg.act(s, epsilon=epsilon)
 
 				s_p, r, d, info = env.step(a)
 				if t==steps-1: d = True
@@ -42,7 +37,7 @@ def train(env, ddpg, epochs=10, episodes=200, steps=100, epsilon=0.3,
 				s = s_p
 
 				if d:
-					ddpg.discount_reward(t)
+					disc_r.append(ddpg.discount_reward(t))
 					reward.append(r)
 					break
 		
@@ -50,7 +45,7 @@ def train(env, ddpg, epochs=10, episodes=200, steps=100, epsilon=0.3,
 
 		
 		
-		ddpg.update(iter=5, n=100)
+		ddpg.update(iter=20, n=50)
 		#run(ddpg, env, episodes=1, steps=200)
 
 		#ddpg.clear_buffer()
@@ -61,21 +56,30 @@ def train(env, ddpg, epochs=10, episodes=200, steps=100, epsilon=0.3,
 			plt.ylabel("Reward")
 
 			
-			plt.plot(np.linspace(0, len(ddpg.q_loss), num=len(avg_reward)).tolist(),
-				np.array(avg_reward)/abs(np.max(avg_reward)), label="Reward")
+			ls1 = np.linspace(0, len(ddpg.q1_loss), num=len(avg_reward)).tolist()
+			avg_rp = np.array(avg_reward)
+			plt.plot(ls1, avg_rp/np.max(np.abs(avg_rp)), label="Reward")
 
-			q_loss = np.array(ddpg.q_loss)
-			q_loss = q_loss/q_loss.max()
+			q1_loss = np.array(ddpg.q1_loss)
+			q1_loss = q1_loss/q1_loss.max()
+
+			q2_loss = np.array(ddpg.q2_loss)
+			q2_loss = q2_loss/q2_loss.max()
 			
-			p_loss = np.array(ddpg.p_loss)/200
+			p_loss = np.array(ddpg.p_loss)
+			p_loss = p_loss/abs(np.max(np.abs(p_loss)))
 
-			plt.plot(q_loss, label="Q loss")
-			plt.plot(p_loss, label="P loss")
+			plt.plot(q1_loss, label="Q1 loss")
+			plt.plot(q2_loss, label="Q2 loss")
 
-			disc_reward = ddpg.buffer.discount_reward_buffer
-			disc_reward = disc_reward/abs(np.max(disc_reward))
+			ls2 = np.linspace(0, len(ddpg.q1_loss), num=len(p_loss)).tolist()
+			plt.plot(ls2, p_loss, label="P loss")
 
-			#plt.plot(disc_reward, label="Discount Reward")
+			disc_rp = np.array(disc_r)
+			disc_rp = disc_rp/np.max(np.abs(disc_rp))
+			ls3 = np.linspace(0, len(ddpg.q1_loss), num=len(disc_rp)).tolist()
+			
+			plt.plot(ls3, disc_rp, label="Discount Reward")
 
 			plt.legend()
 			plt.draw()
@@ -101,7 +105,8 @@ def run(ddpg, env, episodes=10, steps=1000):
 		for t in range(steps):
 
 			env.render()
-			a = ddpg.act(s)
+			a = ddpg.act(s, epsilon=0.3)
+			print(a)
 			s, r, d, info = env.step(a)
 			if t==steps-1: d = True
 
@@ -112,7 +117,7 @@ def run(ddpg, env, episodes=10, steps=1000):
 	env.close()
 
 
-env = gym.make('Humanoid-v3')
+env = gym.make('LunarLanderContinuous-v2')
 
 s_size=env.observation_space.shape[0]
 a_size=env.action_space.shape[0]
@@ -121,12 +126,10 @@ print("State Size:", s_size)
 print("Action Size:", a_size)
 
 ddpg = DDPG(in_dim=s_size, out_dim=a_size, p_alpha=1e-3, q_alpha=1e-3)
-reward = train(env, ddpg, epochs=1000, episodes=1,
- steps=250, epsilon=0.1, render=False, graph=True, run=False)
+reward = train(env, ddpg, epochs=100, episodes=1,
+ steps=200, epsilon=0.1, render=False, graph=True, run=False)
 
-#print(ddpg.p_loss)
-
-run(ddpg, env, steps=1000)
+run(ddpg, env, episodes=3, steps=200)
 
 plt.plot(reward/np.max(reward), label="Reward")
 plt.plot(np.array(ddpg.q_loss)/np.max(ddpg.q_loss), label="Q loss")
