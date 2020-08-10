7646b69bbc41ed459662c621a20ae9595f6879a0
Gregory Eales <gregoeales@aol.com>
2020-08-10 07:19:51 +0100

    Edit main.py

--- Deep-Boltzmann-Machine/main.py
+++ Deep-Boltzmann-Machine/main.py
@@ -1,1 +1,28 @@
+import torch
+import numpy as np
+from tqdm import tqdm 
+
+
+class DBM(torch.nn.Module):
+
+	def __init__(self):
+		
+
+		self.optimizer = torch.optim.Adam()
+
+	def forward(self, x):
+		pass
+
+	def loss(self, x):
+		pass
+
+	def optimize(self, x, y, epochs):
+		
+		
+		for i in tqdm(range(epochs), "Training Model"):
+			pass
+			
+	
+
+
 
